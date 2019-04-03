a41dd0a8d0d0ebce8e3cbf2d9f5af03f010cefab
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-02 20:31:47 -0600

    West Virigina health facility

---
+++ 04-April/day-02/westvirigin.py
@@ -0,0 +1,19 @@
+url = "https://ohflac.wvdhhr.org/Apps/Lookup/FacilitySearch"
+
+import requests
+from bs4 import BeautifulSoup
+result = requests.get(url)
+
+print(result.status_code)
+c = result.content
+
+soup = BeautifulSoup(c, "lxml")
+
+#print(soup.findAll(id="content"))
+
+#print(soup.head)
+
+#print(soup.findAll('state'))
+
+#print(soup.find_all('state'))
+print(soup.text)
