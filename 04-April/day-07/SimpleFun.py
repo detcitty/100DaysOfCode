5e75f324043b100471415355466332dae61696a6
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-07 15:37:09 -0600

    Final Simple Fun solution

--- 04-April/day-07/SimpleFun.py
+++ 04-April/day-07/SimpleFun.py
@@ -1,12 +1,15 @@
+# https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/python
+
 def reverse_letter(string):
     #do your magic here
     text = ""
     list_name = range(len(string))
     len_str = len(string)
     for i in list_name:
-        print(i)
+        # print(i)
         text += string[len_str - (i + 1)]
 
-    return(text)
+    final = ''.join(filter(str.isalpha, text))
+    return(final)
 
 print(reverse_letter("krishan"))
