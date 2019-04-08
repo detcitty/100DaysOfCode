d8d94c993777461cc3a22ea3c5ab65388ab613ce
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-07 21:07:30 -0600

    Codewars practice

---
+++ 04-April/day-07/middle.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/find-the-middle-element/train/python
+
+def gimme(input_array):
+    # Implement this function
+    if(input_array[0] < input_array[1]):
+        if(input_array[0] > input_array[2]):
+            return(0)
+        else:
+            return
+    elif(input
