f2ef527e4f7562abd88222017b8bb40a1578450c
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-08 06:55:23 -0600

    Final solution remove_smallest

--- 04-April/day-08/RemoveMinimum.py
+++ 04-April/day-08/RemoveMinimum.py
@@ -1,13 +1,23 @@
 # https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/python
 
 def remove_smallest(numbers):
-    num = min(numbers)
-    index = Null
-    for i in range(len(numbers)):
-        if(numbers[i] == num):
-            index = i
-            break
-    return(numbers.remove(index))        
+    num = 0
+    index = 0
+    remove_numbers = []
+    Found = False
+
+    if(len(numbers) == 0):
+        return(numbers)
+    else:
+        num = min(numbers)
+            #print(num)
+        for i in range(len(numbers)):
+            if(numbers[i] == num and Found == False):
+                Found = True
+                continue
+            remove_numbers.append(numbers[i])
+        #print(numbers)
+    return(remove_numbers)
 
 
 remove_smallest([1, 2, 3, 4, 5])
