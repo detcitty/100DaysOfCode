22e623ef52cb62f276bb72d4d4e76da59c66f459
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-10 21:19:40 -0600

    Modify comments

--- 04-April/day-10/thirt.py
+++ 04-April/day-10/thirt.py
@@ -4,7 +4,7 @@ def thirt(n):
     # your code
     remainders = [1, 10, 9, 12, 3, 4]
     delimited_num = [int(i) for i in str(n)]
-    print(delimited_num)
+    # print(delimited_num)
     sum = 0
     for i in map(int, str(n)):
         step = remainders[i % 6]
