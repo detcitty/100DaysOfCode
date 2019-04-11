f45150db9c606c030162cfa31f7c648e94f9f779
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-10 21:15:49 -0600

    Use a list comprehension generator

--- 04-April/day-10/thirt.py
+++ 04-April/day-10/thirt.py
@@ -3,6 +3,8 @@
 def thirt(n):
     # your code
     remainders = [1, 10, 9, 12, 3, 4]
+    delimited_num = [int(i) for i in str(n)]
+    print(delimited_num)
     sum = 0
     for i in map(int, str(n)):
         step = remainders[i % 6]
