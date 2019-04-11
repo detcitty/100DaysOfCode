656df31fd1a7157685b6165b29b2f5fa0529b32f
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-10 20:59:58 -0600

    Codewars test problem - day 10

---
+++ 04-April/day-10/thirt.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/564057bc348c7200bd0000ff/train/python
+
+def thirt(n):
+    # your code
+    remainders = [1, 10, 9, 12, 3, 4]
+
+    numbers = n.split("")
+
+    for i in range(len(n)):
+
+
+thirt(8529)
