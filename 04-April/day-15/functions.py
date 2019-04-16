bc5a33bc5b6d88bc7330e8a086015163b12939b6
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-15 19:34:57 -0600

    Functional python

---
+++ 04-April/day-15/functions.py
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+
+def zero(): #your code here
+    return 0
+def one(): #your code here
+    return 1
+def two(): #your code here
+    return 2
+
+def three(): #your code here
+    return 3
+def four(): #your code here
+    return 4
+def five(): #your code here
+    return 5
+def six(): #your code here
+    return 6
+def seven(): #your code here
+    return 7
+def eight(): #your code here
+    return 8
+def nine(): #your code here
+    retrun 9
+
+def plus(): #your code here
+    
+def minus(): #your code here
+def times(): #your code here
+def divided_by(): #your code here
