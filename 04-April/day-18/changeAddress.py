4d647f0b36eb47edeea044e94c8f590ad5785eb6
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-18 20:48:29 -0600

    Binary problem

---
+++ 04-April/day-18/changeAddress.py
@@ -0,0 +1,20 @@
+# https://www.codewars.com/kata/52ea928a1ef5cfec800003ee/train/python
+
+def binary(bit):
+    start = bit
+    rounds = []
+    remainder = []
+    while(start > 2):
+        start = int(round(start / 2))
+        left_over = start % 2
+
+        rounds.append(start)
+        remainder.append(left_over)
+
+
+def ip_to_int32(ip):
+  # your code here
+
+  separated = ip.split(".")
+
+print(binary(128))
