ce829ba5f888879866b172f281bacceea51b9728
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-19 22:37:25 -0600

    modifications

--- 04-April/day-19/multipy.py
+++ 04-April/day-19/multipy.py
@@ -1,8 +1,8 @@
 def multiplication_table(row,col):
     # Good Luck!
     table = [[] for x in range(row)]
-    for i in range(col):
-        for j in range(row):
+    for i in range(row):
+        for j in range(col):
             value = (i+1) * (j+1)
             table[i].append(value)
             # table[j][i] = value
