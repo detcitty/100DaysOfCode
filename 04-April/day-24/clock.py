be57bf4f70ade9be3f25689442324cc5a5adc638
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-24 20:20:38 -0600

    Day 24

--- 04-April/day-24/clock.py
+++ 04-April/day-24/clock.py
@@ -6,5 +6,11 @@ def what_is_the_time(time_in_mirror):
     minute = int(time[1])
     print(hour)
     print(minute)
+    hour_mirror = 12 - (hour % 12) - 1
+    minute_mirror = 60 - (minute % 60)
+    print(hour_mirror)
+    print(minute_mirror)
 
 what_is_the_time("06:35")
+what_is_the_time("11:59")
+what_is_the_time("04:00")
