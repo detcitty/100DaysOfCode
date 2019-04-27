c5dd4f6d898e6d9bee99e4074ab6ffe9bb6571e1
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-26 21:26:01 -0600

    Problem 2

---
+++ 04-April/day-26/incrementer.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/string-incrementer/train/python
+import re
+
+def increment_string(strng):
+    re.search('^[A-Za-z])', strng)
+    return strng
+
+print(increment_string("foobar001"))
