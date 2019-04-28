e78108767374d76fe6ca7cfe4b370e91b30d7b9c
Devin Etcitty <devin.etcitty@gmail.com>
2019-04-27 19:50:23 -0600

    Work

--- 04-April/day-27/diophantine.py
+++ 04-April/day-27/diophantine.py
@@ -2,4 +2,16 @@
 
 def sol_equa(n):
     # your code
-    return []
+    values = []
+    for i in range(n):
+
+        x1 = n - 2*i
+        x2 = n + 2*i
+
+        total = x2 * x1
+        print(x1, x2, total)
+        if(x1 == x2):
+            values.append((x1, i))
+    return values
+
+print(sol_equa(12))
