3f8a6b2aa69b6378e4122efd81d2de3604dafd31
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-03 18:32:00 -0600

    Decipher

---
+++ 05-May/day-33/decipher.py
@@ -0,0 +1,13 @@
+import re
+
+def decipher_this(string):
+    #your code here
+    words = string.split(" ")
+    for w in words:
+        first = re.match('\d+', w).group(0)
+        second = re.search('\D+', w).__getitem__()
+        print(first, "This is work", second)
+
+    return words
+
+print(decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"))
