c992ce6bc1ec2fe53c9cb30194db19d6e74dd555
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-09 19:08:08 -0600

    Day 38 practice

---
+++ 05-May/day-39/smiley.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/583203e6eb35d7980400002a/train/python
+import re
+
+def count_smileys(arr):
+    for value in arr:
+        match = re.match('([:;])(=?[~-])', value)
+
+        if()
+
+    return #the number of valid smiley faces in array/list
+
+count_smileys([':D',':~)',';~D',':)'])
