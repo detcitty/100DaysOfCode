947c9f659c900945e8961e5a6daa5e372fcb99ab
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-11 16:56:55 -0600

    Test

--- 05-May/day-41/square.py
+++ 05-May/day-41/square.py
@@ -1,12 +1,21 @@
 # https://www.codewars.com/kata/54eb33e5bc1a25440d000891/train/python
+import queue
 
 def decompose(n):
     # your code
-    values = []
+    values = queue.Queue()
     for i in range(n):
-        values.append(i**2)
+        values.put(i**2)
     
     return values
+
+    last = values.get()
+
+    diff = 1
+    while diff > 0:
+        output = values.get()
+        stage_diff = last - output
+        
 
 
 print(decompose(30))
