a14cf23ac5d20d3faaa11b548153df415d295cee
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-14 19:31:37 -0600

    Final solution

--- 05-May/day-44/cubes.py
+++ 05-May/day-44/cubes.py
@@ -1,1 +1,19 @@
 # https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/python
+
+def find_nb(m):
+    # your code
+
+    index = 1
+    sum = 0
+
+    while (sum < m):
+        cube = index**3
+        sum += cube
+        index += 1
+
+    value = 0
+    if(sum == m):
+        value = index - 1
+    else:
+        value = -1
+    return value
