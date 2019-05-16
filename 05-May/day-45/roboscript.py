af2257c75ab1ffaf456f0a223c2a4d34af222d94
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-15 22:31:24 -0600

    working on day 45

--- 05-May/day-45/roboscript.py
+++ 05-May/day-45/roboscript.py
@@ -1,1 +1,7 @@
 # https://www.codewars.com/kata/58708934a44cfccca60000c4/train/python
+
+import re
+
+def highlight(code):
+    # Implement your syntax highlighter here
+    math = re.findall("F", code)
