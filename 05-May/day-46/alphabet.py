6389453180859419898ad23d176469f6f9e2da6f
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-16 18:22:11 -0600

    Day 46

---
+++ 05-May/day-46/alphabet.py
@@ -0,0 +1,26 @@
+def alphabet_war(fight):
+    #your code here
+
+    letters = flight.split("")
+    previous = ""
+    current = ""
+    next = ""
+
+    index = 0
+
+    while (index < len(letters)):
+
+
+        astrik = "*"
+        if(current == astrik and index > 0 and index < len(letters) - 1):
+            if(next = astrik):
+                letters[i-1] = ""
+                letters[i] == ""
+            else:
+                letters[i-1] = ""
+                letters[i] = ""
+                letters[i+1] = ""
+        elif(current == astrik and index = 0 and index < len(letters) - 1):
+            letters[i-1] =
+
+        index += 1
