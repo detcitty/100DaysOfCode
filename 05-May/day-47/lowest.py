6caed059901bcf548734322b675e3628ae3e583c
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-18 15:34:19 -0600

    List comprehension

--- 05-May/day-47/lowest.py
+++ 05-May/day-47/lowest.py
@@ -1,1 +1,8 @@
 # https://www.codewars.com/kata/554e52e7232cdd05650000a0/train/python
+from collections import deque
+
+def lowest_product(input):
+    ls = [s for s in input]
+    print(ls)
+    return "Number is too small"
+lowest_product("123456789")
