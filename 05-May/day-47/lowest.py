1b1667ffdf8d082bf5774ebed851214f3ec3e00e
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-18 15:45:30 -0600

    Answerish

--- 05-May/day-47/lowest.py
+++ 05-May/day-47/lowest.py
@@ -2,7 +2,18 @@
 from collections import deque
 
 def lowest_product(input):
-    ls = [s for s in input]
-    print(ls)
-    return "Number is too small"
+    ls = [int(s) for s in input]
+    deq = deque(ls)
+    values = deque()
+
+    while (len(deq) is not 0):
+        if (len(values) != 4):
+            values.append(deq.popleft())
+        else:
+            sum = 0
+            for i in values:
+                sum += i
+            print(sum)
+
+
 lowest_product("123456789")
