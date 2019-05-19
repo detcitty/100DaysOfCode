c46ae674acba5ffcf17e251986cbc0801939bd20
Devin Etcitty <devin.etcitty@gmail.com>
2019-05-19 17:37:19 -0600

    Test with day 48

--- 05-May/day-48/simple.py
+++ 05-May/day-48/simple.py
@@ -1,1 +1,24 @@
 # https://www.codewars.com/kata/57a153e872292d7c030009d4/train/python
+
+def simple_transposition(text):
+    # your code here
+
+    size = int((len(text) / 2) + 0.5)
+
+    if(len(text) % 2 != 0):
+        text += " "
+
+    first_row = []
+    second_row = []
+
+    for i in range(size):
+        
+        
+        first_row.append(text[i])
+        second_row.append(text[size+i])
+    print(size)
+    print(first_row)
+    print(second_row)
+    #return
+
+simple_transposition("Simple transposition")
