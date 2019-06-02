32422a4d3c5a244c0e67824693f048ea63708f74
Devin Etcitty <devin.etcitty@gmail.com>
2019-06-01 21:48:24 -0600

    Working problem

--- 06-June/day-54/unique.py
+++ 06-June/day-54/unique.py
@@ -6,25 +6,27 @@ def find_uniq(arr):
 
     first = unique[0]
     second = unique[1]
-
+    #print(first, second)
     count_first = 0
     count_second = 0
 
     value = 0
-    for e in unique:
+    for e in arr:
+        print(e)
         if(e == first):
             count_first += 1
         elif(e == second):
             count_second += 1
 
-        elif(count_first > 1):
-            value = first
-            break
-        elif(count_second > 1):
-            value = second
-            break
+    
+    if(count_first > 2):
+        value = first
+    elif(count_second > 2):
+        value = second
+            
 
 
     return value   # n: unique integer in the array
 
 print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+print(find_uniq([ 0, 0, 0.55, 0, 0 ]))
