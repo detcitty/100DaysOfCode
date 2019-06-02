0428d6d7734cb9e81303dad0c1fc4d995cbe3728
Devin Etcitty <devin.etcitty@gmail.com>
2019-06-02 17:15:27 -0600

    Work

---
+++ 06-June/day-55/auto.py
@@ -0,0 +1,31 @@
+# https://www.codewars.com/kata/5389864ec72ce03383000484/train/python
+
+import re
+
+def autocomplete(input_, dictionary):
+    #your code here
+    found = []
+    for e in dictionary:
+        z = re.match(input_+ ".+", e)
+
+        if z:
+            found.append(z.group())
+
+    print(found)
+
+    if (len(found)> 5):
+        return found[:5]
+    else:
+        return found
+
+dictionary=[ 'abnormal',
+  'arm-wrestling',
+  'absolute',
+  'airplane',
+  'airport',
+  'amazing',
+  'apple',
+  'ball' ]
+
+autocomplete('ai', dictionary), ['airplane','airport']
+autocomplete('a', dictionary), ['abnormal','arm-wrestling','absolute','airplane','airport']
