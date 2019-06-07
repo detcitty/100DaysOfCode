84d0009903de41b3c369b199c6783c942cabb90f
Devin Etcitty <devin.etcitty@gmail.com>
2019-06-06 21:54:10 -0600

    Not done, imaginary numbers

--- 06-June/day-57/simpson.py
+++ 06-June/day-57/simpson.py
@@ -1,4 +1,9 @@
 # https://www.codewars.com/kata/565abd876ed46506d600000d/train/python
+import math
+
+def f(x):
+    return 3/2 * math.sin(x)**3
 
 def simpson(n):
     # your code
+    h = (math.pi - 0) / float(n)
