43aff225a2ca33dda07939bcb660a99c5007720a
Devin Etcitty <devin.etcitty@gmail.com>
2020-03-18 21:28:21 -0600

    Day 1: Hard problem

---
+++ 2020/03-March/2020-03-18/car.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/554a44516729e4d80b000012/train/python
+'''
+Problem: I'm not sure I understand what's going on.
+The example is not exactly clear.
+Maybe this would be a nice one to revisit in the future.
+'''
+def nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth):
+    # your 
+    
+    
+    return []
