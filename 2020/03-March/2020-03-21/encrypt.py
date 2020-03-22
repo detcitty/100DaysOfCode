252956d3c19ff36085f608a41010686280e05804
Devin Etcitty <devin.etcitty@gmail.com>
2020-03-21 20:07:42 -0600

    Test

--- 2020/03-March/2020-03-21/encrypt.py
+++ 2020/03-March/2020-03-21/encrypt.py
@@ -2,18 +2,22 @@ def encrypt_this(text):
 
     words = text.split(" ")
 
+
     encrypt = ""
-    for w in words:
-        first = w[:1]
-        print(first)
-        last = w[-1:]
-        print(last)
-        second = w[1:2]
+
+    if (text != ""):
+        for w in words:
+
+            first = w[:1]
+            #print(first)
+            last = w[-1:]
+            #print(last)
+            second = w[1:2]
 
 
-        new_word = str(ord(first)) + last + w[2:len(w)-1] + second
+            new_word = str(ord(first)) + last + w[2:len(w)-1] + second
 
-        encrypt += new_word + " "
-    return(encrypt)
+            encrypt += new_word + " "
+    return(encrypt.strip())
 
 print(encrypt_this("A wise old owl lived in an oak"))
