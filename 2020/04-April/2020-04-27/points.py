a820848601bf71339bbdb9a2a61e438244db2a6b
Devin Etcitty <devin.etcitty@gmail.com>
2020-04-27 18:55:11 -0600

    Remove default.profraw

--- 2020/04-April/2020-04-27/points.py
+++ 2020/04-April/2020-04-27/points.py
@@ -1,8 +1,8 @@
 # https://www.codewars.com/kata/5bb904724c47249b10000131/train/python
 
 def points(games):
-	split = lambda x: x.split(":")
-    values = map(split, games)
+    split = lambda x: x.split(":")
+    values = list(map(split, games))
     return(values)
 
 test = ['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']
