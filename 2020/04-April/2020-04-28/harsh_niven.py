c1a43437e4d758ebfc3848b0dcba245ef7339ffd
Devin Etcitty <devin.etcitty@gmail.com>
2020-04-28 18:27:32 -0600

    Practice class

---
+++ 2020/04-April/2020-04-28/harsh_niven.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/54a0689443ab7271a90000c6/train/python
+
+class Harshad:
+
+    @staticmethod
+    def is_value(number):
+        pass
+
+    @staticmethod
+    def get_next(number):
+        pass
+
+    @staticmethod
+    def get_series(count, start=0):
+        pass
