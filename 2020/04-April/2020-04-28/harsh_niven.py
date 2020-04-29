d4ff204c1e196fb6a715721cf8568c894a537ff0
Devin Etcitty <devin.etcitty@gmail.com>
2020-04-28 19:07:24 -0600

    Not done

--- 2020/04-April/2020-04-28/harsh_niven.py
+++ 2020/04-April/2020-04-28/harsh_niven.py
@@ -4,12 +4,44 @@ class Harshad:
 
     @staticmethod
     def is_value(number):
-        pass
+        var = list(str(number))
+        back_to_int = lambda x: int(x)
+
+        numbers = list(map(back_to_int, var))
+        sum_num = sum(numbers)
+        mod = number % sum_num
+        return(mod == 0)
 
     @staticmethod
     def get_next(number):
-        pass
+        found = False
+        n = number + 1
+        while (found == False):
+            check = Harshad.is_value(n)
+            if (check):
+                found = True
+            else:
+                n += 1
+        return(n)
+
 
     @staticmethod
     def get_series(count, start=0):
-        pass
+        list_values = []
+        found = 0
+        num = start + 1 if start==0 else start
+        while(found < count):
+
+            is_harsh = Harshad.is_value(num)
+            while(is_harsh == False):
+                num = Harshad.get_next(num+1)
+                is_harsh = True
+            list_values.append(num)
+            found += 1
+        return(list_values)
+
+
+
+print(Harshad.is_value(1001))
+print(Harshad.get_next(17))
+print(Harshad.get_series(3))
