136c03c1169e562a66e3b0c4a18b3d701f84154c
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-09 06:41:08 -0700

    2/09/21

--- 2021/practice.py
+++ 2021/practice.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import time
 
 values = []
@@ -9,4 +9,7 @@ print(values)
 
 print(time.strftime("%A", time.localtime()))
 
-print(list(map(lambda x: 2**2, list(range(33)))))
+print(list(map(lambda x: 2**x, list(range(33)))))
+
+sin_x = np.range(100)
+print(np.numpy(sin_x))
