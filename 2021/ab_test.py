97de609f673d6107e0b9b4953fb63f5d07178fa7
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-14 18:38:33 -0800

    Practice

---
+++ 2021/ab_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import  statsmodels.stats.api as sms
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from math import ciel
+
+%matplotlib inline
+
+
