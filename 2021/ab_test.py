7683bc99f884ba17ead47ceb0a1a4d23ad0f7184
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-15 10:26:49 -0800

    2/15/2021

--- 2021/ab_test.py
+++ 2021/ab_test.py
@@ -5,8 +5,26 @@ import  statsmodels.stats.api as sms
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
-from math import ciel
+from math import ceil
 
-%matplotlib inline
+#%matplotlib inline
 
+plt.style.use('seaborn-whitegrid')
+font = {
+    'family' : 'Helvetica',
+    'weight' : 'bold',
+    'size'   : 14
+    }
+mpl.rc('font', **font)
+effect_size = sms.proportion_effectsize(0.13, 0.15)
 
+required_n = sms.NormalIndPower().solve_power(
+    effect_size, 
+    power=0.8, 
+    alpha=0.05, 
+    ratio=1
+    )
+
+required_n = ceil(required_n)
+
+print(required_n)
