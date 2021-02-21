ece1a3f454ee90187c70fdf58e40cfc578f2c8a1
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-21 16:03:50 -0700

    2/21/21

---
+++ 2021/codewars/gapprime.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4/train/python
+
+def gap(g, m, n):
+    is_prime = []
+    # I don't know how to find prime numbers efficently
+    for i in range(1, n):
+        if n % 2 == 0:
+            is_prime.add(i)
+    return(is_primee)
+
+print(gap(1, 3, 14))
