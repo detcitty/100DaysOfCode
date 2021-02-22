0b0890ecf0ea7e872863547ac72ad4d07572fa02
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-21 17:18:12 -0700

    Test

--- 2021/codewars/gapprime.py
+++ 2021/codewars/gapprime.py
@@ -3,9 +3,7 @@
 def gap(g, m, n):
     is_prime = []
     # I don't know how to find prime numbers efficently
-    for i in range(1, n):
-        if n % 2 == 0:
-            is_prime.append(i)
-    return(is_prime)
+    odd = [x if x % 2 != 0 else '' for x in range(n)]
+    return(odd)
 
 print(gap(1, 3, 14))
