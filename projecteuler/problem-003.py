b9984c21147b23b729a5c917c0fc0f251e2e464b
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-22 07:51:39 -0700

    2/22/21

--- projecteuler/problem-003.py
+++ projecteuler/problem-003.py
@@ -2,9 +2,12 @@ import numpy as np
 
 end_ = 600851475143
 end_2 = 10
+add_array = []
 for i in range(end_2):
     values = np.arange(0, i)
     round_ = values % i
-    print(round_)
+    if i % 2 != 0:
+        add_array.append(i)
+    print(add_array)
 
 x = map(lambda x, y: x %y, np.arange(20), np.arange(20))
