a0cc77942671f83326ad7d62a2a146cbf96c8e0d
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-24 08:16:59 -0700

    2/23/21

--- projecteuler/problem-003.py
+++ projecteuler/problem-003.py
@@ -7,10 +7,12 @@ for i in range(1, end_2):
     if (i <= 2 or i % 2 != 0):
         prime_numbers.append(i)
         for p in prime_numbers:
-            if (i > 2 or i % p  != 0):
+            if (i > 2 or i % p  == 0):
                 prime_numbers.append(i)
             else:
                 continue
+    else:
+        continue
     print(prime_numbers)
 
 x = map(lambda x, y: x %y, np.arange(20), np.arange(20))
