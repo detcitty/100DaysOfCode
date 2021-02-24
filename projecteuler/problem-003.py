8ff45dc16f579ec6a10c8cad1ef6058903cfd957
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-23 20:27:09 -0700

    Update

--- projecteuler/problem-003.py
+++ projecteuler/problem-003.py
@@ -2,10 +2,15 @@ import numpy as np
 
 end_ = 600851475143
 end_2 = 10
-add_array = []
-for i in range(end_2):
-    if (i == 2 or i % 2 != 0):
-        add_array.append(i)
-    print(add_array)
+prime_numbers = []
+for i in range(1, end_2):
+    if (i <= 2 or i % 2 != 0):
+        prime_numbers.append(i)
+        for p in prime_numbers:
+            if (i > 2 or i % p  != 0):
+                prime_numbers.append(i)
+            else:
+                continue
+    print(prime_numbers)
 
 x = map(lambda x, y: x %y, np.arange(20), np.arange(20))
