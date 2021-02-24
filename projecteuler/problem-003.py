479387883fdabccd6f208d27434e8cf758e70324
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-23 20:04:36 -0700

    ProjectEuler

--- projecteuler/problem-003.py
+++ projecteuler/problem-003.py
@@ -4,9 +4,7 @@ end_ = 600851475143
 end_2 = 10
 add_array = []
 for i in range(end_2):
-    values = np.arange(0, i)
-    round_ = values % i
-    if i % 2 != 0:
+    if (i == 2 or i % 2 != 0):
         add_array.append(i)
     print(add_array)
 
