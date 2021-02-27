ae95374f26630aaadcc2cefeecb67bc605b0d13a
Devin Etcitty <devin.etcitty@gmail.com>
2021-02-27 10:39:55 -0700

    2/27/21

---
+++ 2021/codewars/diff_volumne.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/python
+
+def find_difference(a, b):
+    # Your code here!
+    total_a = 1
+    total_b = 1
+    for i in range(3):
+        total_a *= a[i]
+        total_b *= b[i]
+
+    return(total_a - total_b)
