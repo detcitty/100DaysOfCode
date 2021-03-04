7cf81a69ace5f492dbe6ac1a297112d6f637fd61
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-03 18:12:30 -0700

    fixed

--- 2021/codewars/revse.py
+++ 2021/codewars/revse.py
@@ -2,7 +2,13 @@
 
 def reverse_words(s):
     words = s.split(" ")
+    print(words)
     flipped_s = ""
-    for i in range(len(s)):
-        flipped_s += words[len(s)-i-1]
+    for i in range(len(words)):
+        print(i)
+        index_opp = len(words) - i - 1
+        print(index_opp)
+        flipped_s += words[index_opp] + " "
     return flipped_s
+
+print(reverse_words("Devin is a cunt!"))
