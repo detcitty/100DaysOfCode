91105ad9659b876f81780e2553e4fe828a0e0d29
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-10 18:27:15 -0700

    Classes

--- 2021/codewars/ball-class.py
+++ 2021/codewars/ball-class.py
@@ -1,5 +1,8 @@
 # https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/python
 
 class Ball(object):
-    def __init__(self, object):
-        self.ball_type = object
+    def __init__(self, name=object):
+        if name=="":
+            self.ball_type = 'regulars'
+        else:
+            self.ball_type = object
