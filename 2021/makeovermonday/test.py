d90ff01cdc4847ab60e39029f4d80690f18ba479
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-15 17:52:53 -0600

    3/15/21

--- 2021/makeovermonday/test.py
+++ 2021/makeovermonday/test.py
@@ -1,4 +1,9 @@
-import dataworld
+#%pylab inline
+import pandas as pd
 import os
 
-# Get Data.world ENV api credentials
+# Import the datadotworld module as dw
+import datadotworld as dw
+# First
+# Import the city council votes dataset
+dataset = dw.load_dataset('stephen-hoover/chicago-city-council-votes')
