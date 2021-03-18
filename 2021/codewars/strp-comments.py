86b2f3072569d10975d93764720df3e549b06d93
Devin E <devin.etcitty@gmail.com>
2021-03-18 13:36:50 -0600

    update

--- 2021/codewars/strp-comments.py
+++ 2021/codewars/strp-comments.py
@@ -1,4 +1,11 @@
 # https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/python
+import re
 
 def solution(string,markers):
     #your code here
+    remove_regex = r'[\#\!](.*)([\n$])'
+    final = re.sub(remove_regex, "\2", string, flags=re.M)
+    return(final)
+
+te = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+print(te)
