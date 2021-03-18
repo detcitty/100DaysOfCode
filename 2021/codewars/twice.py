a047e629689dc713d5e8127e825d146e1e01466e
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-17 19:59:08 -0600

    3/17/21

---
+++ 2021/codewars/twice.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5b853229cfde412a470000d0/train/python
+
+def twice_as_old(dad_years_old, son_years_old):
+    sons_double = son_years_old * 2
+
+    diff_ = abs(sons_double - dads_years_old)
+
+    return(diff_)
