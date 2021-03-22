4c781f8a4daf972b0bc1e7af96f076c27b0034da
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-22 00:24:11 -0600

    Test

--- 2021/codewars/rangex.py
+++ 2021/codewars/rangex.py
@@ -2,16 +2,22 @@
 
 def solution(args):
 
-    # i = 0
-    # end_i = len(args)
+    i = 0
+    end_i = len(args)
     new_array = []
     count = []
-    for i in range(len(args)-1):
+    clear = []
+    while i < end_i - 1:
         print(i)
         if (args[i]+1 == args[i+1]):
-            count.append(i)
+            clear.append(i)
+            i += 1
         else:
+            values = ' '.join([str(elem) for elem in clear]) 
             new_array.append(args[i])
+            new_array.append(values)
+            clear = []
+            i += 1
     return(new_array)
 
 values = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
