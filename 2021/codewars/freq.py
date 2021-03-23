61deec28fcc6b71fc81cd3f8922d249946b6cd9c
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-22 19:37:13 -0600

    Practice

--- 2021/codewars/freq.py
+++ 2021/codewars/freq.py
@@ -8,4 +8,11 @@ def top_3_words(text):
     all_words = cleaned_now.split(" ")
     return(all_words)
 
+test_ = """In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income."""
 
+print(top_3_words(test_))
