05c2b9964be04856f078993a0b0276cb68d2ddb6
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-25 13:29:22 -0600

    3/25/21

---
+++ 2021/codewars/1ncycle.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5a057ec846d843c81a0000ad/train/python
+
+def cycle(n):
+    values = []
+    for i in range(n):
+        decimals = 1/float(n)
+        values.append(decimals)
