3016401e83aa98c6282872300663ec8e059ca028
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-24 21:50:58 -0600

    Practice

---
+++ 2021/codewars/slout.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/52f677797c461daaf7000740/train/python
+
+def solution(a):
+    first = a[1] - a[0]
+    second = a[2] - a[0]
+    third = a[2] - a[1]
+    return # smallest possible sum of all numbers
