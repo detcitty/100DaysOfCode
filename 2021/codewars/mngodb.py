dd0ecfba88f8816f4dfd0072610ddbfebb4434e9
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-27 07:36:09 -0600

    3/27/21

---
+++ 2021/codewars/mngodb.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/52fefe6cb0091856db00030e/train/python
+
+from datetime import datetime
+
+class Mongo(object):
+
+    @classmethod
+    def is_valid(cls, s):
+        """returns True if s is a valid MongoId; others False"""
+        pass
+
+    def get_timestamp(cls, s):
+        """if s is a MongoID, returns a datetime object"""
+        pass
+
