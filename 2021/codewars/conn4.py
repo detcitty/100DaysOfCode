47a2b1fda0b2a1a53b569fdde47b72608f207086
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-29 08:18:26 -0600

    3/29/21

---
+++ 2021/codewars/conn4.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/586c0909c1923fdb89002031/train/python
+
+class Connect4():
+
+    def __init__(self):
+        # code here
+
+        pass
+
+    def play(self, col):
+        # code here
+        pass
+
