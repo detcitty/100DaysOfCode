65f88db6c869d9a18a8a12e421e2bde34aea8118
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-30 08:46:14 -0600

    Practice

---
+++ 2021/codewars/tapcode.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/605f5d33f38ca800322cb18f/train/python
+
+def tap_code_translation(text):
+    split = text.split("")
+    for i in split:
+        print(i)
+
+test = "Devin is practicing"
+
+tap_code_translation(test)
