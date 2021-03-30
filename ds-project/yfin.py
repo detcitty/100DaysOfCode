4848ed7f075fa8f5afcd9fd855ae4bcbc3ac740d
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-30 08:23:31 -0600

    More practice

--- ds-project/yfin.py
+++ ds-project/yfin.py
@@ -1,7 +1,18 @@
 import yfinance as yf
 import pprint
+import pandas_datareader.data as wb
+from datetime import datetime
+
+start = datetime(2016, 9, 1)
+end = datetime(2019, 9, 1)
+
+#matches = wb.DataReader('F', 'iex', start, end)
 
 pp = pprint.PrettyPrinter(indent=4)
 
 msft = yf.Ticker("MSFT")
-pp.pprint(msft.info)
+victory = yf.Ticker("VACI")
+x_10 = yf.Ticker("VCVC")
+
+#pp.pprint(x_10._download_options())
+pp.pprint(x_10.major_holders())
