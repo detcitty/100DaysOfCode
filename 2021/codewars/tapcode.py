15235eb517335d5ff44885a95c8d177719045c8d
Devin Etcitty <devin.etcitty@gmail.com>
2021-03-31 08:56:52 -0600

    Practice

--- 2021/codewars/tapcode.py
+++ 2021/codewars/tapcode.py
@@ -1,7 +1,7 @@
 # https://www.codewars.com/kata/605f5d33f38ca800322cb18f/train/python
 
 def tap_code_translation(text):
-    split = text.split("")
+    split = text.split(r'\s')
     for i in split:
         print(i)
 
