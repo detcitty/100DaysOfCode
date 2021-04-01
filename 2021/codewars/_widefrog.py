4ec6c2e4f2ef1cf1629e509f9a412dcd70cdc9df
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-01 07:27:59 -0600

    practice

---
+++ 2021/codewars/_widefrog.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/python
+
+def mouth_size(animal):
+
+    if animal == 'alligator':
+        return("small")
+    else:
+        return("wide")
+
+print(mouth_size("ant"))
