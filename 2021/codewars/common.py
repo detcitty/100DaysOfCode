c15c1bd1b86d384b367a6d252363c809947f05e3
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-03 12:49:31 -0600

    practice

--- 2021/codewars/common.py
+++ 2021/codewars/common.py
@@ -1,6 +1,12 @@
+import math
+
 def convertFracts(lst):
-	all_denoms = list(map(lambda x: x[1], list))
+	all_denoms = list(map(lambda x: x[1], lst))
+	final_denom = math.prod(all_denoms)
+	all_num = list(map(lambda x: ( final_denom * x[0] )/ x[1], lst))
+	print(all_num)
 	return(all_denoms)
 
-val = convertFracts([1, 2], [1, 3], [1, 4])
+
+val = convertFracts([[1, 2], [1, 3], [1, 4]])
 print(val)
