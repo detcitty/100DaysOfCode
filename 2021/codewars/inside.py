ad17f167e96157c0a6037d486ff5583e2c9103dd
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-06 06:26:14 -0600

    Practice

--- 2021/codewars/inside.py
+++ 2021/codewars/inside.py
@@ -1,12 +1,18 @@
 # https://www.codewars.com/kata/57ebdf1c2d45a0ecd7002cd5/train/python
 
 def inside_out(st):
-    values = list(st)
+
+    
+    
     end = ''
-    while len(values) != 0:
-        end += values.pop(-1)
+    
+    for e in st.split(" "):
+        values = list(e)
+        while len(values) != 0:
+            end += values.pop(-1)
+        end += " "
 
-    return(end)
+    return(end.strip())
 
-test = "Devin"
+test = "man i need a taxi up to ubud"
 print(inside_out(test))
