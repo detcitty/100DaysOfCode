49656dab539854b48ad87241563763fe727bb48d
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-07 07:17:12 -0600

    solved

--- 2021/codewars/solved/dutyfree.py
+++ 2021/codewars/solved/dutyfree.py
@@ -2,7 +2,7 @@ def duty_free(price, discount, holiday_cost):
     perc = price * (discount / 100.0)
     other = holiday_cost / perc
 
-    return(other)
+    return(int(other))
 
 test = duty_free(12, 50, 1000)
 print(test)
