7851c95dbd68f0cc0cb2b89c5f5d56231dbc8ccf
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-12 08:37:48 -0600

    Update

--- 2021/codewars/wait.py
+++ 2021/codewars/wait.py
@@ -1,8 +1,8 @@
 # https://www.codewars.com/kata/5875b200d520904a04000003/train/python
 
 def enough(cap, on, wait):
-    left_over = on - cap
+    left_over = cap - on
 
-    diff = left_over - wait
+    diff = wait - left_over
 
-    return(0 if diff > 0 else abs(diff))
+    return(0 if diff < 0 else abs(diff))
