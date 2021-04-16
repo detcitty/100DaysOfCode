f48ea74228b877354b16602d383755b032c9b57f
Devin Etcitty <devin.etcitty@gmail.com>
2021-04-16 08:13:23 -0600

    4/16/21

---
+++ kaggle/ex1.py
@@ -0,0 +1,21 @@
+# https://www.kaggle.com/detcitty/exercise-strings-and-dictionaries/edit
+
+def word_search(doc_list, keyword):
+    """
+    Takes a list of documents (each document is a string) and a keyword.
+    Returns list of the index values into the original list for all documents
+    containing the keyword.
+
+    Example:
+    doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+    >>> word_search(doc_list, 'casino')
+    >>> [0]
+    """
+    for doc in doc_list:
+        lower_doc = doc.lower()
+
+        bag = lower_doc.split(" ")
+        for b in bag:
+            if bag.isspace() or bag.isalpha():
+                continue
+            else:
