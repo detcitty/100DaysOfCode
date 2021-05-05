4db32b339e86030a864a4cbd75d7f2daadced52c
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-05 15:50:32 -0600

    5/5/21

---
+++ 2021/codewars/greet.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+def greet(name):
+    #Good Luck (like you need it)
+    return("Hello, {} how are you doing today?".format(name))
