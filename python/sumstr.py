10aab51234f5ca6e29b4d5e2af37fd0f11fb454c
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-08 09:56:08 -0600

    Test

--- python/sumstr.py
+++ python/sumstr.py
@@ -2,4 +2,7 @@
 
 def sum_str(a, b):
     # happy coding !
-    return(str(sum([int(a), int(b)])))
+    int_a = 0 if not a else int(a)
+    int_b = 0 if not b else int(b)
+
+    return(str(sum([int_a, int_b])))
