2f17d8d872a2d988ecc9eeb1c4b84de77ddaaab8
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-10 22:22:26 -0600

    5/10/21

---
+++ python/grav.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/python
+
+def flip(d, a):
+    # Do some magic
+    sort_left = True if d == "L" else False
+    a.sort(reverse=sort_left)
+    return(a)
