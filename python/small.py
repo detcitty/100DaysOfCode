8ab0fdb55e25154846bb1c59e0b16277efc0c542
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-13 10:49:49 -0600

    5/13/21

---
+++ python/small.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57cc981a58da9e302a000214/train/python
+
+def small_enough(array, limit):
+    compare = list(map(lambda x: x <= limit, array))
+    print(compare)
+    is_small = all(compare)
+    return(is_small)
+values = small_enough([1,2,3,5,6,7,10], 6)
+print(values)
+
