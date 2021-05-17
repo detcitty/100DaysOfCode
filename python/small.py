b61eb9f31fa421e6d4210168fd32a53bcc873a72
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-16 23:01:42 -0600

    5/16/21

--- python/small.py
+++ python/small.py
@@ -4,6 +4,7 @@ def small_enough(array, limit):
     compare = list(map(lambda x: x <= limit, array))
     print(compare)
     is_small = all(compare)
+    print(is_small)
     return(is_small)
 
 values = small_enough([1,2,3,5,6,7,10], 6)
