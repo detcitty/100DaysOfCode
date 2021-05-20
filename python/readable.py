5d6bc866fb3f263b07223c948eb669e80d21b69f
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-20 11:29:33 -0600

    5/20/21

---
+++ python/readable.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/53f17f5b59c3fcd589000390/train/python
+
+class SecureList():
+  # Implement here...
+  def __init__(self, list_messages):
+  	self.list_messages = list_messages
