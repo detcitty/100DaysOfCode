e329b4b0433ebf5afbe27114da7de28f1e5aed06
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-22 20:45:55 -0700

    5/22/21

---
+++ python/learn.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5695995cc26a1e90fe00004d/train/python
+
+# class Machine:
+    def command(self, cmd, num):
+        return map(cmd(num))
+    def response(self,res):
+        return(res)
+
