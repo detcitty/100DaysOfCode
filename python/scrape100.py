acca763552661e9c9b2c0fdcb605b9c07f64341e
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-23 12:14:11 -0700

    5/23/21

---
+++ python/scrape100.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/581c06b95cfa838603000435/train/python
+
+from bs4 import BeautifulSoup
+
+URL = 'https://www.codewars.com/users/leaderboard'
+
+class Leaderboard:
+    pass
+
+class User(Leaderboard):
+    pass
+
+def solution():
+  # do it
+  return
+
