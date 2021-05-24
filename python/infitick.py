7d46fbaedcb681dd86c9e4ab272012d42a768804
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-24 16:55:27 -0700

    5/24/21

---
+++ python/infitick.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/58817056e7a31c2ceb000052/train/python
+
+def interpreter(tape):
+    # Your code here
+
---
+++ python/tick.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/587edac2bdf76ea23500011a/train/python
+
+def interpreter(tape):
+    # Your code here
+    values = tape.split()
+    unique_values = set(values)
+
+    found = {}
+
+    for e in values:
+        found[e] += 1
+
+    return(found)
+
+
