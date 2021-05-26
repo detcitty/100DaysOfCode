8c192252247806422778fdef0e8a4952b79fbf52
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-25 18:26:32 -0700

    5/25/21

---
+++ python/isimp.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/556b85b433fb5e899200003f/train/python
+
+def mixed_fraction(s):
+    num, denom = split("/")
+    whole_ = int(int(num) / int(denom))
+    remainder_ = int(num) % int(denom))
+    return None
