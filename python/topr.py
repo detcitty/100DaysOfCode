ba3c70880b6acac90e00f70f283db9b9647793f8
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-27 09:09:34 -0700

    5/27/21

---
+++ python/topr.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5838b5eb1adeb6b7220000f5/train/python
+
+import pandas as pd
+
+def id_best_users(*args):
+    # your code here
+    values = pd.read_data(args)
+    return [[]]
