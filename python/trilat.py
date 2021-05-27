4fd7358028c66f36f3ace69f38781fb44807b5bc
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-26 18:00:00 -0700

    5/26/21

---
+++ python/trilat.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57cebf1472f98327760003cd/train/python
+
+def count_col_triang(input_):
+    # your code here
+    colors = list(map(lambda x: x[-1]), input_)
+    all_colors = set(colors)
+    return [[]]
