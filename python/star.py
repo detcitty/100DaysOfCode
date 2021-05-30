2b45f38302208aea44280f5756ac2a0ce949e6cb
Devin Etcitty <devin.etcitty@gmail.com>
2021-05-29 18:57:31 -0700

    5/29/21

--- python/star.py
+++ python/star.py
@@ -1,6 +1,10 @@
 # https://www.codewars.com/kata/5865a407b359c45982000036/train/python
+from itertools import combinations
 
 def slogan_maker(array):
     #your code here
     first = list(set(array[0]))
-    second = array[1]
+    # second = array[1]
+    all_comb = list(combintations(first, len(first)))
+    return(all_comb)`
+
