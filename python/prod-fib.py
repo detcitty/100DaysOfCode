3a0f970a487dc9c0bf3e88807d7673d848b2cf22
Devin E <devin.etcitty@gmail.com>
2021-06-03 21:32:28 -0600

    3/03/21

---
+++ python/prod-fib.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5541f58a944b85ce6d00006a/train/python
+
+def productFib(prod):
+    factors = []
+    for i in range(1, prod + 1):
+        if prod % i == 0:
+            factors.append(i)
+    return(factors)
