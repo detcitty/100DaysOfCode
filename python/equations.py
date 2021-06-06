2a9f289601d88894e2c7a92081d88721221b20b3
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-06 16:35:28 -0600

    6/6/21

---
+++ python/equations.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/59280c056d6c5a74ca000149/train/python
+
+def solve_eq(eq):
+    # your code here
+    values = list(map(lambda x, y, z: sum([x, y, z], eq))
+    return []
