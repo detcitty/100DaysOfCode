b83ede496c804af0f407a47f9c9a70d369770b42
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-10 11:15:16 -0600

    6/10/21

---
+++ python/codingsqr.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/56fcc393c5957c666900024d/train/python
+
+def code(s):
+    # your code
+    values = list(map(lambda x: x.split(" "), c))
+
+def decode(s):
+    # your code
+
