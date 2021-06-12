54e526893698983ea841cce93a4cf2adc3634d53
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-12 10:37:33 -0600

    6/12/21

---
+++ python/dycls.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/55dec8f72ead8624e5000028/train/python
+
+
+def create_class(class_name, secrets):
+    class class_name:
+        def __ini__():
+            self.secrets = secrets
