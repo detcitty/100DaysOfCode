1b452ada1ad4637bc0a52a57079ba428155a0d4b
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-16 19:44:27 -0600

    6/16/21

---
+++ python/snlad.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/587136ba2eefcb92a9000027/train/python
+
+class SnakesLadders():
+
+    def __init__(self):
+        # Code Here
+        w, h = 10, 10
+        Matrix = [[0 for x in range(w)] for y in range(h)]
+        self.board = Matrix
+
+    def play(self, die1, die2):
+        # Code Here
