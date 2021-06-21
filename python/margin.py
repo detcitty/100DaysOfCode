7128331debb968f60f8e813dfa230f7f216d4e9f
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-21 17:23:08 -0600

    6/21/21

---
+++ python/margin.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/56453a12fcee9a6c4700009c/train/python
+
+def close_compare(a, b, margin):
+    diff_ = abs(a - b)
+
+    if margin > diff_:
+        if a > b:
+            pass
+        else:
+            pass
+    else:
+        pass
+
