df2c692321fc8ea20ef4c633abbdec27b336c1b2
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-23 22:33:49 -0600

    6/23/21

---
+++ python/traflight.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/58649884a1659ed6cb000072/train/python
+
+def update_light(current):
+    # Your code here
+    final = ''
+
+    if current == 'green':
+        pass
+    elif current == 'yellow':
+        pass
+    else:
+        pass
+    return(final)
