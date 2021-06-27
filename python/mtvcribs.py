b21a115eb086a24645b669687d2c22e9bc52afcc
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-27 13:04:01 -0600

    6/27/21

---
+++ python/mtvcribs.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5834a44e44ff289b5a000075/train/python
+
+def my_crib(n):
+    #your code here
+    values = []
+    for i in range(len(n)):
+        row = []
+        for j in range(len(n)):
+            row.append("_")
+        values.append(values)
