cfedf0b7da92ebb8cd8ddf99338e00a2ca6f99c3
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-26 20:12:11 -0600

    6/26/21

---
+++ js/sum.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/dashboard
+
+// Sum Numbers
+function sum (numbers) {
+	var sum = 0;
+	for(int i = 0; i < numbers.length; i++) {
+		sum += numbers[i];
+	}
+	return(sum);
+};
