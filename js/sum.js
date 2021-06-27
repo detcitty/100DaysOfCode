aa0fa4a3f973e794f03108306051d294481df975
Devin Etcitty <devin.etcitty@gmail.com>
2021-06-26 20:13:08 -0600

    Update

--- js/sum.js
+++ js/sum.js
@@ -3,7 +3,7 @@
 // Sum Numbers
 function sum (numbers) {
 	var sum = 0;
-	for(int i = 0; i < numbers.length; i++) {
+	for(var i = 0; i < numbers.length; i++) {
 		sum += numbers[i];
 	}
 	return(sum);
