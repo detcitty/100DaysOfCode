25dd3b8559fd18e9b53b87036ad20d487346a8ee
Devin E <devin.etcitty@gmail.com>
2021-07-01 07:55:16 -0600

    Fixed, maybe

--- python/lastdigit.py
+++ python/lastdigit.py
@@ -2,9 +2,11 @@
 
 def last_digit(n):
 	value = 1
-	for i in range(n):
+	for i in range(1, n+1):
 		value = value * i
 	num_str = str(value)
-	num_no_zeros = num_str.remove('0')
-	lastdigit = num_no_zeros[-1]
-    return(int(lastdigit))
+	num_no_zeros = num_str.replace('0', '')
+	lastdigit = list(num_no_zeros)[-1]
+	return(int(lastdigit))
+
+print(last_digit(3))
