f054cd0b8347e61acaff497ac81538ae075b0370
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-02 16:31:31 -0600

    Update

--- python/twicelinear.py
+++ python/twicelinear.py
@@ -4,14 +4,15 @@ def dbl_linear(n):
 	values = [1]
 	start = 0
 
-	while(len(values) <= n):
+	while(len(values) < n):
+		values.sort()
 		y = 2 * values[start] + 1
 		z = 3 * values[start] + 1 
 		values.append(y)
 		values.append(z)
 		start += 1
-	values.sort()
+		values.sort()
 	return(values)
 
-test = dbl_linear(10)
+test = dbl_linear(20)
 print(test)
