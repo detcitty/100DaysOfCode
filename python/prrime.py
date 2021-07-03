9b8791276e352488b5670706545fe80ecd589c86
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-03 07:37:17 -0600

    7/3/21

---
+++ python/prrime.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/54d512e62a5e54c96200019e/train/python
+
+def prime_factors(n):
+	start = 2
+	reduced_value = n:
+	values = []
+	while(reduced_value != 1):
+		if reduced_value % start == 0:
+			reduced_value = reduced_value / 2.0
+			values.append(values)
+		else if:
+			pass
+		else:
+			pass
