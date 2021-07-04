8e27adbd643f9fab1901c72bd6701a4d61a2e31d
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-04 09:27:33 -0600

    7/4/21

---
+++ python/primnumreverse.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55ea170313b76622b3000014/train/python
+
+def same_factRev(nMax): # nMax upper bound of the range to study
+    # your code here
+    index = 1
+    step = 1
+    value = nMax / step
+    return [] # sorted list with the found numbers bellow nMax
