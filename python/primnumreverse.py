eebc2e4f6544344b1ec86fcff28c6fa7c4fc17b8
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-04 09:38:59 -0600

    Work in progress

--- python/primnumreverse.py
+++ python/primnumreverse.py
@@ -1,8 +1,27 @@
 # https://www.codewars.com/kata/55ea170313b76622b3000014/train/python
 
+
+def find_divide_mod(value, step):
+    divide_ = value / step
+    mod_ = value % step
+    return[divde_, mod_]
+
 def same_factRev(nMax): # nMax upper bound of the range to study
     # your code here
     index = 1
     step = 1
-    value = nMax / step
+    value_divide, value_mod = find_divide_mod(nMax, step)
+    found_primes = []
+    large_prime_limit = sqrt(nMax)
+    while()
+
+    while (value_divide != 1):
+
+        if (value_divide != value):
+            value_divide, value_mod = find_divide_mod(value_divide, step+1)
+        else if ():
+            pass
+        else:
+            pass
+
     return [] # sorted list with the found numbers bellow nMax
