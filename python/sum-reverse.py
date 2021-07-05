2cca600cc2e2b1016260fc14e6fa9f05834ee17b
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-05 14:39:33 -0600

    7/05/21

---
+++ python/sum-reverse.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5603a9585480c94bd5000073/train/python
+
+def memoize_help(n):
+	# I need to do research
+	return(list)
+
+def sum_dif_rev(n):
+	#your code here
+	return x # n-th term of the sequence
