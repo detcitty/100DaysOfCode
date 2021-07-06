fba890734fc77ab6c71fa185a81f9802417d7de5
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-06 07:38:00 -0600

    Update

--- python/pokerende.py
+++ python/pokerende.py
@@ -1,7 +1,20 @@
 # https://www.codewars.com/kata/52ebe4608567ade7d700044a/train/python
 
 def encode(cards):
+    cards = {
+    'c' : 0,
+    'd' : 1,
+    'h' : 2, 
+    's' : 3
+    }
     return
 
 def decode(cards):
+    cards = {
+    0 : 'c',
+    1 : 'd',
+    2 : 'h',
+    3 : 's'
+    }
+
     return
