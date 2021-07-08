b2b5e6df92ca8a79ed54f33641b53e1fe76988c0
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-08 07:52:38 -0600

    7/08/21

---
+++ python/maxsub.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/56e3cbb5a28956899400073f/train/python
+
+def find_subarr_maxsum(arr):
+	#your code Here
+	max_num = max(arr)
+	max_indices = [y if x == max_num else None for x, y in enumerate(arr)]
+	print(max_indices)
+	return [[], x] # or [[[]], x]
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+test1 = find_subarr_maxsum(arr)
