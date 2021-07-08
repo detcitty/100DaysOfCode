00b91d625565ec12381fb46cf018ca0057acbe08
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-07 18:38:51 -0600

    7/7/21'

---
+++ python/countinginner.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/53efc28911c36ff01e00012c/train/python
+
+def count_calls(func, *args, **kwargs):
+  """Count calls in function func"""
+
+  rv = func(*args, **kwargs)
+  
+  return calls, rv
+
