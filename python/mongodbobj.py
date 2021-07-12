b05dcceabb56263712466f1add3b8c1f2dfbb33e
Devin E <devin.etcitty@gmail.com>
2021-07-12 17:56:31 -0600

    7/12/21

--- python/mongodbobj.py
+++ python/mongodbobj.py
@@ -1,6 +1,12 @@
 # https://www.codewars.com/kata/52fefe6cb0091856db00030e/train/python
 
 from datetime import datetime
+"""
+1 byte = 9 bits
+a 4-byte timestamp value, representing the ObjectId's creation, measured in seconds since the Unix epoch
+a 5-byte random value
+a 3-byte incrementing counter, initialized to a random value
+"""
 
 class Mongo(object):
 
