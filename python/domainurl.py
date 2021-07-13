9039a45d1e4db747e988b5cf4a91309e6cfbcb4d
Devin E <devin.etcitty@gmail.com>
2021-07-13 17:28:49 -0600

    Completed

--- python/domainurl.py
+++ python/domainurl.py
@@ -2,12 +2,14 @@
 import re
 
 def domain_name(url):
-	beginning_regex = r'(http[s]?)?(\:\/+)?(w{3}\.?)'
-	mod_string = re.sub(beginning_regex, '', url, re.IGNORECASE)
-	return(mod_string)
+	beginning_regex = r"^(http[s]?)?(\:\/+)?(w{3}\.?)?"
+	mod_string = re.sub(beginning_regex, '', url, flags=re.I)
+	next_part = r"^[^\.]*"
+	begin_name = re.match(next_part, mod_string, flags=re.I)
+	return(begin_name[0])
 
 
-test1 = domain_name("http://google.com")
+test1 = domain_name("http://www.google.com")
 test2 = domain_name("http://google.co.jp")
 test3 = domain_name("www.xakep.ru")
 test4 = domain_name("https://youtube.com")
