a2a8e4711830f8146f451ef6b3327e0c9f565ec1
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-14 21:30:06 -0600

    Update

--- python/htmlgen.py
+++ python/htmlgen.py
@@ -4,4 +4,7 @@ import random
 def generate_color_rgb():
     #here is the code go
     #Go go go go
-    return None
+    rgb = list(map(lambda x: random.randint(0, x), [256] * 3))
+    return rgb
+
+print(generate_color_rgb())
