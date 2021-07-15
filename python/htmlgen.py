ce70c1b2589dac332c97e8b6ca1665cb502d025b
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-14 21:20:28 -0600

    7/14/21

---
+++ python/htmlgen.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56f1c6034d0c330e4a001059/train/python
+
+import random
+def generate_color_rgb():
+    #here is the code go
+    #Go go go go
+    return None
--- python/overtheroad.py
+++ python/overtheroad.py
@@ -2,3 +2,10 @@
 
 def over_the_road(address, n):
 	even_ = True if address % 2 == 0 else False
+
+	if even_:
+		odd_list = list(range(address-1, 2*n, 2))
+		even_list = list(range(address, address-2*n, -2))
+	else:
+		odd_list = list(range(address, 2*n, 2))
+		even_list = list(range(address+1, address-2*n, -2))
