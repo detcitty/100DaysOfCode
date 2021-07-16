b211146c16bee423d1452ce1517d13199ff1ef35
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-15 18:40:51 -0600

    update

--- python/pypassval.py
+++ python/pypassval.py
@@ -1,3 +1,3 @@
 # https://www.codewars.com/kata/52e1476c8147a7547a000811/train/python
 
-regex = r'([A-Za-z0-9]{6,}|[a-z]|[A-Z]|[0-9])'
+regex = r'([A-Za-z0-9]{6,}[A-Za-z0-9]{6,}[A-Za-z0-9]{6,})(?<![^A-Za-z0-9])'
