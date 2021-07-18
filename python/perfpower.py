0bec482769f59fe623d3e9059c944c294206309e
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-17 23:19:28 -0600

    7/17/21

---
+++ python/perfpower.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54d4c8b08776e4ad92000835/train/python
+
+def isPP(n):
+	#your code here
+	# Can I do a prime decomposition?
+	potential_primes = list(map(lambda x: x , ))
+
+pp = [4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484]
