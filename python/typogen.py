4d58183107779a5c9e6e558afa7172d7ab258f62
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-18 13:37:51 -0600

    Update

--- python/typogen.py
+++ python/typogen.py
@@ -1,4 +1,5 @@
 # https://www.codewars.com/kata/55953e906851cf2441000032/train/python
+import re
 
 def scramble_words(words):
 	#your code here
@@ -7,7 +8,21 @@ def scramble_words(words):
 
 	for w in words_list:
 		word_indices = list(range(len(w)))
+		found_indices = []
 
-		for char, i in enumerage(w):
+		for i, chars in enumerage(w):
+			alpha_regex = re.compile(r"[a-zA-Z]", flags=re.I)
+			found_indices.append(i if i not in [0, len(w)-1] or alpha_regex.search(chars) else "")
+
+		clean_indices = list(filter(None, found_indices))
+		relevant_char = sorted(list(map(lambda word_str, idx: word_str[idx], w, clean_indices)))
+
+		word_assembled = ""
+		for j in found_indices:
 			pass
+	
 	return(new_string)
+
+test_ = scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.")
+
+print(scramble_words)
