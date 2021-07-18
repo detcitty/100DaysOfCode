7c1ee067176a4c458510e5cfcb5a04a195275517
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-17 23:26:14 -0600

    Update

--- python/perfpower.py
+++ python/perfpower.py
@@ -3,6 +3,7 @@
 def isPP(n):
 	#your code here
 	# Can I do a prime decomposition?
-	potential_primes = list(map(lambda x: x , ))
+	odds = list(range(1, n+1, 2))
+	potential_primes = list(map(lambda x: x**2, odds))
 
 pp = [4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484]
