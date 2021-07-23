0f0e23153191b25f605ffb75d4fe8639c057861d
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-23 09:40:27 -0700

    7/23/21

---
+++ python/snd-cipher.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/55084d3898b323f0aa000546/train/python
+
+import regex as re
+
+def encode_str(strng, shift):
+    # your code
+    char_regex = r"[A-Za-z]"
+
+def decode(arr):
+    #your code
+    alpha_regex = r"[0-9]"
