a5b55d3e43a571e7a2efee6c37031f0fca027f1b
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-25 11:09:06 -0700

    Update

--- python/stringcount.py
+++ python/stringcount.py
@@ -2,5 +2,9 @@
 import re
 def mix(s1, s2):
     # your code
-    char_regex = r"[^a-z]"
+    char_regex = re.compile(r"[^a-z]")
     list_str = list(map(lambda x, y: re.split(x, y), [s1, s2], char_regex))
+    return(list_str)
+
+test1 = mix("Are they here", "yes, they are here")
+print(test1)
