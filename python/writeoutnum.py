33b08a4dbb84bb3bc16fd09c954ef087623abfc1
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-26 21:22:19 -0700

    7/26/21

---
+++ python/writeoutnum.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/52724507b149fa120600031d/train/python
+
+def number2words(n):
+    """ works for numbers between 0 and 999999 """
+    map_numbers = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"
+    }
