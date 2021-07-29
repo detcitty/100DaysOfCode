d1ba99753cea27b46564e972bff5c9b7133a38ab
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-28 17:47:25 -0700

    Update

--- python/rot13.py
+++ python/rot13.py
@@ -1,4 +1,5 @@
 # https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
 
 def rot13(message):
-    list(map(lambda x: ord(x) + 13, massage.split()))
+    values = list(map(lambda x: chr(ord(x) + 13), list(message)))
+    return(values)
