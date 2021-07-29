12bfbd7d24b1be7f5b2a74bca1b3a3c5d17e1e3b
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-29 10:35:33 -0700

    Update

--- python/intrec.py
+++ python/intrec.py
@@ -2,7 +2,8 @@
 
 def list_squared(m, n):
     # your code
-    return list(map(lambda x: x*2, [m, n]))
+    squared = list(map(lambda x: x*2, range(m, n+1)))
+    filter(lambda x, y: x == y,squared)
 
 test1 = list_squared(1, 250)
 print(test1)
