10b2726ea018a3683a8384a2af215b331f9687a9
Devin Etcitty <devin.etcitty@gmail.com>
2021-07-30 14:58:18 -0700

    7/30/21

---
+++ python/wouldyoubel.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/57fd6c4fa5372ead1f0004aa/train/python
+
+def siegfried(week, txt):
+    # Your code here!
+
+    week1 = { 'ci' : 'si',
+            'ce': 'se',
+            'c' : 'k'}
+
+    week2 = {'ph' : 'f'}
+
+    week4 = {'th': 'z', 
+            'wr': 'r',
+            'wh': 'v',
+            'w': 'v'}
+
+    week5 = {'ou': 'u',
+            'an': 'un',
+            'ing': 'ink',
+            'sm': 'schm'}
+
