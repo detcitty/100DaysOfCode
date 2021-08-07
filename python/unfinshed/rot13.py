b8a03af9e3c122f413b5802a3101e76c854d5aca
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-07 16:56:50 -0600

    Update

--- python/unfinshed/rot13.py
+++ python/unfinshed/rot13.py
@@ -1,5 +1,6 @@
 # https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
 
 def rot13(message):
-    values = list(map(lambda x: chr(ord(x) + 13), list(message)))
+    values = list(map(lambda x: chr(ord(x) + 13) + (chr(x) + 93) % 13, list(message)))
+    values
     return(values)
