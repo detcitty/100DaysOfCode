aebf45697e6f32436ea0a3fe6100db21c620d095
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-08 20:00:20 -0600

    Final solution

--- python/unfinshed/maxsubarray.py
+++ python/unfinshed/maxsubarray.py
@@ -3,7 +3,7 @@ import numpy as np
 from itertools import combinations
 
 def max_sequence(arr):
-    index_values_tuples = list(combinations(range(len(arr)), 2))
+    index_values_tuples = list(combinations(range(len(arr)+1), 2))
     index_values = list(map(lambda x: slice(x[0], x[1]), index_values_tuples))
     # print(index_values)
     found_sums = []
@@ -11,7 +11,9 @@ def max_sequence(arr):
         sub_array = arr[e]
         found_sums.append(sum(sub_array))
     
-    return(max(found_sums))
+    is_empty = True if len(arr) == 0 else False
+
+    return(max(found_sums) if not is_empty and max(arr) >= 0 else 0)
 
 test1 = max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 print(test1)
