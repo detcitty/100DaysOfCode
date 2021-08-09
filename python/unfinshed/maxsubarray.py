da6dcd5782a0945bad3bd8506dea0d1d93581325
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-08 19:51:18 -0600

    Update

--- python/unfinshed/maxsubarray.py
+++ python/unfinshed/maxsubarray.py
@@ -3,14 +3,15 @@ import numpy as np
 from itertools import combinations
 
 def max_sequence(arr):
-    index_values = list(combinations(range(len(arr)), 2))
-    print(index_values)
+    index_values_tuples = list(combinations(range(len(arr)), 2))
+    index_values = list(map(lambda x: slice(x[0], x[1]), index_values_tuples))
+    # print(index_values)
     found_sums = []
     for i, e in enumerate(index_values):
         sub_array = arr[e]
         found_sums.append(sum(sub_array))
     
-    return(found_sums)
+    return(max(found_sums))
 
 test1 = max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 print(test1)
