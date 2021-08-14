a45c31c997e96d7119d2e5059f971ca217f7a334
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-14 13:19:33 -0600

    8/14/21

---
+++ python/unfinshed/pushbuild.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55be95786abade3c71000079/train/python
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+def push(head, data):
+    # Your code goes here.
+    pass
+
+def build_one_two_three():
+    # Your code goes here.
+    pass
