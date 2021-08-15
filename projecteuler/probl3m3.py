e62e912d88824f7e09d175408879114c0f45c41d
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-15 12:45:55 -0600

    More updates

--- projecteuler/probl3m3.py
+++ projecteuler/probl3m3.py
@@ -6,11 +6,12 @@ I am trying to figure out how prime numbers work.
 import numpy as np
 
 def start(number):
-    half_way = number / 2.0
-    list_values = np.arange(half_way)
+    half_way = (number + 1) / 2.0
+    list_values = np.arange(half_way, dtype='i2')
     odds = list_values[(list_values % 2 == 1) | (list_values == 2)]
-    return(odds)
+    odds_sans_fives = odds[(odds % 5 != 0) | (odds == 5)]
+    return(odds_sans_fives)
     #map(lambda x, y: x(y), half_way, prime)
 
-test1 = start(10)
+test1 = start(1000)
 print(test1)
