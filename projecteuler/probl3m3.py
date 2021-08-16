382d86602ffe206c19d295d092fe041b7b5d04ca
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-16 14:44:22 -0600

    Update

--- projecteuler/probl3m3.py
+++ projecteuler/probl3m3.py
@@ -9,7 +9,8 @@ def start(number):
     half_way = (number + 1) / 2.0
     list_values = np.arange(half_way, dtype='i2')
     odds = list_values[(list_values % 2 == 1) | (list_values == 2)]
-    odds_sans_fives = odds[(odds % 5 != 0) | (odds == 5)]
+    odds_sans_threes = odds[(odds % 3 != 0) | (odds == 3)] 
+    odds_sans_fives = odds_sans_threes[(odds_sans_threes % 5 != 0) | (odds_sans_threes == 5)]
     return(odds_sans_fives)
     #map(lambda x, y: x(y), half_way, prime)
 
