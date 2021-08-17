72c29dc6c4f70d24dc047692fe75c946bc1c0858
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-17 14:50:55 -0600

    8/17/21

--- projecteuler/probl3m3.py
+++ projecteuler/probl3m3.py
@@ -11,7 +11,8 @@ def start(number):
     odds = list_values[(list_values % 2 == 1) | (list_values == 2)]
     odds_sans_threes = odds[(odds % 3 != 0) | (odds == 3)] 
     odds_sans_fives = odds_sans_threes[(odds_sans_threes % 5 != 0) | (odds_sans_threes == 5)]
-    return(odds_sans_fives)
+    count_files = len(odds_sans_fives)
+    return(count_files)
     #map(lambda x, y: x(y), half_way, prime)
 
 test1 = start(1000)
