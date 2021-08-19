c581d06c5bf20ef9f2382c12af4892f8e9e6e063
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-19 08:12:12 -0600

    8/19/21

--- python/unfinshed/thinkful.py
+++ python/unfinshed/thinkful.py
@@ -2,6 +2,13 @@
 
 def fillable(stock, merch, n):
     # Your code goes here.
+    '''
+    I'm not sure what the code does.
+    I think I will try to understand what is going on.
+
+    But I think this is what happening:
+    - check if the key is available.
+    '''
     final_price = ''
     if merch in stock.keys():
     	available = stock[merch] 
