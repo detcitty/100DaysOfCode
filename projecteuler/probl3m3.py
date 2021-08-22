fd8347f7778c08fc5a0c38c5a7cb62c6e61045ea
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-22 09:55:43 -0600

    8/22/21

--- projecteuler/probl3m3.py
+++ projecteuler/probl3m3.py
@@ -10,14 +10,20 @@ What is the largest prime factor of the number 600851475143 ?
 
 import numpy as np
 
-def start(number):
+def start(number):    
+    '''
+    The code fails when making large computations. 
+    I need to figure out a way how to not use an array.
+
+    What about finding all of the prime numbers and checking to see if it's a multiple of X.
+    '''
     half_way = (number + 1) / 2.0
     list_values = np.arange(half_way, dtype='i2')
     odds = list_values[(list_values % 2 == 1) | (list_values == 2)]
     odds_sans_threes = odds[(odds % 3 != 0) | (odds == 3)] 
     odds_sans_fives = odds_sans_threes[(odds_sans_threes % 5 != 0) | (odds_sans_threes == 5)]
     count_files = len(odds_sans_fives)
-    return(count_files)
+    return(count_files[-1])
 
     #map(lambda x, y: x(y), half_way, prime)
 
