02aa7a2dbd133a6fb29430096dc5917962d96538
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-25 10:37:12 -0600

    8/25/21

--- python/unfinshed/differentiate.py
+++ python/unfinshed/differentiate.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/566584e3309db1b17d000027/train/python
 
 def differentiate(equation, point):
+    '''
+    What is calculus differentiation?
+    '''
     values = equation.split("+")
     exponent = list(map(lambda x: x.split("x^")), values)
     return exponent**2
