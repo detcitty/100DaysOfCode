7438f47feecb085a4f39d151cd7addc8cb4a9f71
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-27 17:22:13 -0700

    8/27/21

--- python/unfinshed/star.py
+++ python/unfinshed/star.py
@@ -1,11 +1,15 @@
 # https://www.codewars.com/kata/5865a407b359c45982000036/train/python
 from itertools import combinations
 
+'''
+What about having good code and commits?
+
+'''
 def slogan_maker(array):
     #your code here
     first = list(set(array[0]))
     # second = array[1]
-    all_comb = list(combintations(first, len(first)))
+    all_comb = list(combinations(first, len(first)))
     concat_str = list(map(lambda x: " ".join(x), all_comb))
     together = '\r\n'.join(concat_str)
     return(together)
