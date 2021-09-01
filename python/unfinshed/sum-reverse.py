9033d046de0cac074d8cb0c8cab874867c19e6a0
Devin Etcitty <devin.etcitty@gmail.com>
2021-08-31 19:44:12 -0700

    8/31/21

--- python/unfinshed/sum-reverse.py
+++ python/unfinshed/sum-reverse.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/5603a9585480c94bd5000073/train/python
 
 def memoize_help(n):
+	'''
+	Memoize: Something that is similar to caching
+	'''
 	# I need to do research
 	return(list)
 
