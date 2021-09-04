e9d23e253fd0efeaf01934aaf7e0b60fd54231c1
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-03 19:13:58 -0600

    9/3/21

--- python/unfinshed/walker.py
+++ python/unfinshed/walker.py
@@ -2,6 +2,10 @@
 
 def solve(a, b, c, alpha, beta, gamma):
     # your code
+    '''
+    This looks like the diff function
+    How would I write this function?
+    '''
     y = m * x + b
     print(y)
     return(y)
