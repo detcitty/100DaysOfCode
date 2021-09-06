b9de25a0c0bdb9e9143916986d2fea68fd67892c
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-05 20:19:51 -0600

    9/5/21

--- python/unfinshed/dycls.py
+++ python/unfinshed/dycls.py
@@ -2,6 +2,10 @@
 
 
 def create_class(class_name, secrets):
+    '''
+    How the hell do I create a function that creates a class?
+    I do not know. 
+    '''
     class class_name:
         def __ini__():
             self.secrets = secrets
