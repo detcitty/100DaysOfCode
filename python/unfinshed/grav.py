8261287cef71b90d37f6be8f9b1878f014faa1dd
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-06 15:12:03 -0600

    9/6/21

--- python/unfinshed/grav.py
+++ python/unfinshed/grav.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/python
 
 def flip(d, a):
+    '''
+    What does this even do?
+    '''
     # Do some magic
     sort_left = True if d == "L" else False
     a.sort(reverse=sort_left)
