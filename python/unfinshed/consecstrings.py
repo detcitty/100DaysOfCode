96dfda742913c2ff85ccdc1a3d1a1dd272a08186
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-07 21:12:42 -0600

    testing

--- python/unfinshed/consecstrings.py
+++ python/unfinshed/consecstrings.py
@@ -6,3 +6,14 @@ def longest_consec(strarr, k):
     # your code
     combos = list(combinations(strarr, k))
     str_joined = map(lambda x: x.join(''), combos)
+    str_lengeth = map(lambda x: str(x), str_joined)
+    res = dict(zip(str_joined, str_lengeth))
+    max_value = max(res.values())
+    str_longest = ''
+
+    for k, v in enumerate(res.items()):
+        if v == max_value:
+            str_longest = k
+            break
+
+    return(str_longest)
