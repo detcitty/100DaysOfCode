efcfdb29e1330503aff47b03ed0a737b7ea2fe76
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-07 20:57:39 -0600

    Update

--- python/unfinshed/clsm.py
+++ python/unfinshed/clsm.py
@@ -1,11 +1,9 @@
 # https://www.codewars.com/kata/5868b2de442e3fb2bb000119/train/python
 
 def closest(string):
-    # your code
     '''
     What does this code do?
     '''
-    
     values = string.split(" ")
     values.sort()
     combo = list(map(lambda x, y: [x, y], values, values))
