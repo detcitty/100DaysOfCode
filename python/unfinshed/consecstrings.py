1abc188c08c4e0c83c0015fbc129c8b5ea21dffe
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-08 18:48:36 -0600

    9/8/21

--- python/unfinshed/consecstrings.py
+++ python/unfinshed/consecstrings.py
@@ -1,4 +1,4 @@
-# https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/javascript
+# https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/python
 
 from itertools import combinations
 
---
+++ python/unfinshed/validdate.py
@@ -0,0 +1,3 @@
+# https://www.codewars.com/kata/548db0bd1df5bbf29b0000b7/train/python
+
+valid_date = r'[01-31]'
