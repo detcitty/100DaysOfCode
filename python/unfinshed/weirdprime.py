5f813cd55e40de08b73dbb2e3821b64cd8e798ad
Devin E <devin.etcitty@gmail.com>
2021-09-14 11:59:56 -0600

    9/14/21

---
+++ python/unfinshed/weirdprime.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/562b384167350ac93b00010c/train/python
+
+import math
+
+def count_ones(n):
+    # your code
+    return(Math.gcd(n, n+2))
+
+def max_pn(n):
+    # your code
+
+def an_over_average(n):
+    # your code
