f309663b387477883e3fded356f443a3e67bf031
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-20 07:08:48 -0600

    More stuff

--- python/unfinshed/fusc.py
+++ python/unfinshed/fusc.py
@@ -3,3 +3,12 @@
 def fusc(n):
     assert type(n) == int and n >= 0
     #Your code here
+
+    if (n == 0):
+        return(0)
+    elif(n == 1):
+        return(1)
+    elif(n % 2 == 0):
+        return(fusc(n))
+    else:
+        return(fusc(n) + fusc(n+1))
