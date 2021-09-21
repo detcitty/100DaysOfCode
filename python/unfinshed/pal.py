f9d158700e76a745379a2d826df0a046a776d309
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-21 07:44:17 -0600

    Power

--- python/unfinshed/pal.py
+++ python/unfinshed/pal.py
@@ -1,10 +1,15 @@
 # https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/python
 
-def move(s_list):
+def move(s_list, end):
     value = True
-    for i in range(0, int(len(s_list)/2)):
-        if(s_list[i] != s_list[i-i]):
+    #print(end)
+    for i in range(0, end):
+        #print(i)
+        if(s_list[i] == s_list[i-i]):
+            continue
+        else:
             value = False
+            #print('here')
             break
     return(value)
 
@@ -12,10 +17,12 @@ def move(s_list):
 def is_palindrome(s):
     is_pal = False
     lowercase_list = list(s.lower())
+    #print(lowercase_list)
     if (len(lowercase_list) % 2 == 0):
-        is_pal = move(lowercase_list)
+        is_pal = move(lowercase_list, int(len(lowercase_list)/2)-1)
     else:
-        is_pal = move(lowercase_list)
+        is_pal = move(lowercase_list, int(len(lowercase_list)/2)+1)
+        #print(is_pal)
     return(is_pal)
 
 test1 = is_palindrome('Kasue')
