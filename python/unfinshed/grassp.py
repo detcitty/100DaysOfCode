22dff4a705a749716b184de665202b7cf7e96653
Devin Etcitty <devin.etcitty@utah.edu>
2021-09-24 20:36:59 -0600

    9/24/21

--- python/unfinshed/grassp.py
+++ python/unfinshed/grassp.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/57089707fe2d01529f00024a/train/python
 
 def check_alive(health):
+    '''
+    how to not be ok? 
+    '''
     if (health <= 0):
         pass
         return False
