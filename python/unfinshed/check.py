8a6794e492f8d42cc1ebc1b0443c579738c71c2d
Devin Etcitty <devin.etcitty@gmail.com>
2021-09-28 22:21:34 -0600

    9/28/21

---
+++ python/unfinshed/check.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/python
+
+def check_exam(arr1,arr2):
+    score = 0
+    for i in range(len(arr1)):
+        if arr1[i] == arr2[i]:
+            score += 4
+        if arr1[i] == "":
+            score += 0
+        else:
+            score -=1
+    return(score)
+            
+  
