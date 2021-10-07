897efd7c5db4222d0619a7d69411b0fbda3b8069
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-07 07:32:20 -0600

    10/7/21

---
+++ python/unfinshed/multable.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/python
+
+def multi_table(number):
+    final_string = ''
+    for i in range(1, number):
+        final_string += "{} * {} = {}".format(i, number, i*number) + '\n'
+    return(final_string)
+
