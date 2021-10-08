c0fdb22516654c2272e8134dd7ec869f7df3fa76
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-08 17:43:40 -0600

    10/8/21

--- python/unfinshed/sumstr.py
+++ python/unfinshed/sumstr.py
@@ -4,5 +4,8 @@ def sum_str(a, b):
     # happy coding !
     int_a = 0 if not a else int(a)
     int_b = 0 if not b else int(b)
+    '''
+    I don't know whawt this is supposed to do.
+    '''
 
     return(str(sum([int_a, int_b])))
