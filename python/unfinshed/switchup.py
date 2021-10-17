b34589f82ac76349e6562655e9749f9109b9e534
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-17 15:00:39 -0600

    More practice

--- python/unfinshed/switchup.py
+++ python/unfinshed/switchup.py
@@ -1,8 +1,10 @@
+#https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
 def switch_it_up(number):
     #your code here
 
     value = {
-        0: 'Zero'
+        0: 'Zero',
         1: 'One',
         2: 'Two',
         3: 'Three',
