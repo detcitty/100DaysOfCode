c938b60df7485477e54baf48c2fc9ad70c090b7a
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-19 21:48:36 -0600

    10/19/21

--- python/unfinshed/equations.py
+++ python/unfinshed/equations.py
@@ -1,5 +1,9 @@
 # https://www.codewars.com/kata/59280c056d6c5a74ca000149/train/python
 
+'''
+Do I know what this function does?
+How can I learn more about generators?
+'''
 def solve_eq(eq):
     # your code here
     values = list(map(lambda x, y, z: sum([x, y, z], eq))
