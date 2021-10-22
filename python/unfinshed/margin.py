2da4a67396da4ef40fd727186f636f4b47d59b71
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-21 22:25:05 -0600

    10/21/21

--- python/unfinshed/margin.py
+++ python/unfinshed/margin.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/56453a12fcee9a6c4700009c/train/python
 
 def close_compare(a, b, margin):
+    '''
+    Whwat does this do? How can I begin to code this?
+    '''
     diff_ = abs(a - b)
 
     if margin > diff_:
