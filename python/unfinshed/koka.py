5578bfc5f4192e2131991bc18495b2fcff663d7e
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-23 11:50:58 -0600

    More practice

--- python/unfinshed/koka.py
+++ python/unfinshed/koka.py
@@ -3,13 +3,17 @@ import re
 
 def kooka_counter(laughing):
     #your code here
-    male_regex = r'Ha'
-    female_regex = r'ha'
+    male_regex = r'HaHaHa'
+    female_regex = r'hahaha'
 
     males = re.findall(male_regex, laughing)
     females = re.findall(female_regex, laughing)
 
     size_males = len(males)
     size_females = len(females)
+    #print(size_males, size_females)
 
-    return(sum(size_females, size_males))
+    return(sum([size_females, size_males]))
+
+test1 = kooka_counter("hahaha")
+print(test1)
