dff0c0f4cdf831a961948f8f77fcd370d6dc8d6d
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-23 11:44:52 -0600

    More updates

--- python/unfinshed/koka.py
+++ python/unfinshed/koka.py
@@ -1,4 +1,15 @@
 # https://www.codewars.com/kata/58e8cad9fd89ea0c6c000258/train/python
+import re
 
 def kooka_counter(laughing):
     #your code here
+    male_regex = r'Ha'
+    female_regex = r'ha'
+
+    males = re.findall(male_regex, laughing)
+    females = re.findall(female_regex, laughing)
+
+    size_males = len(males)
+    size_females = len(females)
+
+    return(sum(size_females, size_males))
