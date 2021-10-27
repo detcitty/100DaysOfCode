c97eda79b496d24b67cc278b4706ee55c79f4bf2
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-26 21:23:36 -0600

    Oh well.

--- python/unfinshed/lastdig.py
+++ python/unfinshed/lastdig.py
@@ -1,8 +1,9 @@
 # https://www.codewars.com/kata/5cd5ba1ce4471a00256930c0/train/python
 
 def solution(n,d):
-    digits = list(map(lambda x: int(n), list(str(n))))
-    return(digits[:d])
+    #print(list(str(n)))
+    digits = list(map(lambda x: int(x), list(str(n))))
+    return(digits[-d+1:])
 
-test1 = solution(123767,4)
+test1 = solution(123,4)
 print(test1)
