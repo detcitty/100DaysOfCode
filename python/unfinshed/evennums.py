02744bb233a4d3ffb1aa1ec5165233198a757695
Devin Etcitty <devin.etcitty@gmail.com>
2021-10-28 21:19:45 -0600

    10/28/21

--- python/unfinshed/evennums.py
+++ python/unfinshed/evennums.py
@@ -1,8 +1,9 @@
 # https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/python
 def even(list_):
+    'I know this works'
     return(list_ % 2 == 0)
 
 def even_numbers(arr,n):
-    
+    'Help me' 
     pass
     
