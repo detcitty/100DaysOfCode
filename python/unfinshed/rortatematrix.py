7c42419b0cd51f0223ceed0e22353048c6706fea
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-09 10:04:08 -0700

    Testing ideas

---
+++ python/unfinshed/rortatematrix.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/525a566985a9a47bc8000670/train/python
+
+def rotate(matrix, direction): 
+    # your code here
+    
+    ## case for even-even rows
+
+    ## case for odd-odd rows
