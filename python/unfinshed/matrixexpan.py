202a1358d6785aa6ddb84329b85013fb8b49a7f5
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-14 08:11:38 -0700

    11/14/21

---
+++ python/unfinshed/matrixexpan.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/614adaedbfd3cf00076d47de/train/python
+
+def expansion(matrix, n):
+    test1 = matrix
+    for i in range(len(matrix)):
+        for j in range(n):
+            test[i].append(sum(test1[:i]))
+    return test1
