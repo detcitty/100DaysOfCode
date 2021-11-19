9fc1509485c912c83e1e79e65d953862230327f6
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-19 09:20:43 -0700

    11/18/21

--- python/unfinshed/rortatematrix.py
+++ python/unfinshed/rortatematrix.py
@@ -1,5 +1,6 @@
 # https://www.codewars.com/kata/525a566985a9a47bc8000670/train/python
 from statistics import median
+import math
 
 def rotate(matrix, direction): 
     # your code here
@@ -9,6 +10,19 @@ def rotate(matrix, direction):
     halfway = median(range(len(matrix)))
     ## case for even-even rows
     for x in range(len(matrix)):
+        row_ = []
         for y in range(len(matrix)):
-            coorindates.append([x-halfway, y-halfway])
+            x_coord = float(x-halfway)
+            y_coord = float(y-halfway)
+            sign_x = math.copysign(1, y_coord)
+            sign_y = math.copysign(1, y_coord)
+
+            if (x_coord.is_integer() and y_coord.is_integer()):
+                row_.append([x_coord, y_coord])
+                #print('PART!')
+            else:
+                x_ciel_abs = math.ceil(abs(x_coord))
+                y_ciel_abs = math.ceil(abs(y_coord))
+                row_.append([sign_x * (x_ciel_abs), sign_y * (y_ciel_abs)])
+        coorindates.append(row_)
     print(coorindates)
