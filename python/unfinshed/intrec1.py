84cbc757ee9e98d7f180a37c6193053963ac12de
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-21 17:50:10 -0700

    9/21/21

---
+++ python/unfinshed/intrec1.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/55aa075506463dac6600010d/train/python
+
+def list_squared(m, n):
+    # your code
+
+
+program_answer, real_answer = (list_squared(250, 500), [[287, 84100]])
