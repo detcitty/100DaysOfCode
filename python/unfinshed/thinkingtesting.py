5fe1cf36aef86d362aa7b1faeb9ca697a95968e1
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-23 09:13:16 -0700

    11/23/21

---
+++ python/unfinshed/thinkingtesting.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/python
+
+def testit (a, b):
+    # Good Luck
+    sum_ = a + b
+    prod_ = a * b
+    return (sum_, prod_) # Hmm is that right?
