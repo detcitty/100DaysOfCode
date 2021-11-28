a0d92a3ad40b75a8c01aa5201d173bc252db95aa
Devin Etcitty <devin.etcitty@gmail.com>
2021-11-28 09:57:54 -0700

    11/28/21

--- python/unfinshed/subtractsum.py
+++ python/unfinshed/subtractsum.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/56c5847f27be2c3db20009c3/train/python
 
 def subtract_sum(number):
+    '''
+    Barbie Blacksheep
+    '''
     numbers = list(lambda x: int(x), list(str(number)))
     return #fruit name like "apple"
 
