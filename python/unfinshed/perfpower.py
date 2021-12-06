a9619ef5bf1bb8a16d75231322f2256aa6adae5e
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-06 11:12:06 -0700

    12/06/21

--- python/unfinshed/perfpower.py
+++ python/unfinshed/perfpower.py
@@ -1,11 +1,13 @@
 # https://www.codewars.com/kata/54d4c8b08776e4ad92000835/train/python
+'''
+Returns a list of prime numbers up to the first one
+'''
+def find_prime_numbers(first_number):
+    find_prime_numbers = [2]
+    start = 3
+    while (start < first_number / 2):
+
+
 
 def isPP(n):
-	#your code here
-	# Can I do a prime decomposition?
-	odds = list(range(1, n+1, 2))
-	potential_primes = list(map(lambda x: x**2, odds))
-	
-	filter(lambda x: isPrime(x), potential_primes))
-
-pp = [4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484]
+    #your code here
