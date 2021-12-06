80272285400a632939756c0ed56b3069c02aa852
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-06 11:24:45 -0700

    Trying to do the code

--- python/unfinshed/perfpower.py
+++ python/unfinshed/perfpower.py
@@ -6,8 +6,12 @@ def find_prime_numbers(first_number):
     find_prime_numbers = [2]
     start = 3
     while (start < first_number / 2):
-
-
+        values = list(map(lambda x, y: x % y, start, find_prime_numbers))
+        if 0 in values:
+            continue
+        else:
+            start += 1
 
 def isPP(n):
     #your code here
+    return()
