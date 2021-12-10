35421203a39395bf0f57ed2914fa793633776f61
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-10 08:50:59 -0700

    More python practice

---
+++ python/unfinshed/currypart.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/53cf7e37e9876c35a60002c9/train/python
+"""
+Also view this website: https://betterprogramming.pub/functional-programming-currying-vs-partial-application-53b8b05c73e3
+It shows good examples: curry vs partial application
+All of this is related to functional programming
+"""
+
+def curry_partial(f,*initial_args):
+    "Curries and partially applies the initial arguments to the function"
