f3857d50965cab1484c4c8e61d346197f82d65c7
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-10 08:46:18 -0700

    Update

--- python/unfinshed/robottacticts.py
+++ python/unfinshed/robottacticts.py
@@ -1,12 +1,18 @@
 # https://www.codewars.com/kata/566b490c8b164e03f8000002/train/python
 from collections import OrderedDict
+import operator
+from functools import reduce
+  
+def countList(lst1, lst2):
+    return reduce(operator.add, zip(lst1, lst2))
 
 def fight(robot_1, robot_2, tactics):
     # your code
+    values = []
     list_of_speeds = list(map(lambda x: x['speed'], [robot_1, robot_2]))
     if list_of_speeds[0] > list_of_speeds[1]:
-        pass
+        tactics1 = countList(robot_1['tactics'], robot_2['tactics'])
     elif list_of_speeds[0] < list_of_speeds[1]:
-        pass
+        tactics2 = countList(robot_2['tactics'], robot_1['tactics'])
     else:
         pass
