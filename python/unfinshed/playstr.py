a2c86e951fa3a876218085935b726760112b9b0c
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-11 09:23:32 -0700

    More updates

--- python/unfinshed/playstr.py
+++ python/unfinshed/playstr.py
@@ -1,7 +1,10 @@
 # https://www.codewars.com/kata/56c30ad8585d9ab99b000c54/train/python
 
 def work_on_strings(a, b):
+    '''
+    How does this work and what do we do for this issues? 
+    '''
     a_list = list(a)
     b_list = list(b)
-    
+
     return list(map(lambda x, y: x == y, a_list, b_list))
