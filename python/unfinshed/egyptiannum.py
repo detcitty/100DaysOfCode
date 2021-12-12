728c4d96be0558bff0783d8335c546d9ef47a138
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-12 10:22:58 -0700

    12/12/21

---
+++ python/unfinshed/egyptiannum.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54f8693ea58bce689100065f/train/python
+import numpy as np
+
+def decompose(n):
+    # your code
+    numbers = np.arrange(10)
+    ones = np.zeros(10) + 1
+    return
