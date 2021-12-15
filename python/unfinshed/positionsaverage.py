145a18ea79be982559de4a34cb3b85597d902068
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-15 09:03:48 -0700

    12/15/2021

--- python/unfinshed/positionsaverage.py
+++ python/unfinshed/positionsaverage.py
@@ -1,8 +1,10 @@
 # https://www.codewars.com/kata/59f4a0acbee84576800000af/train/python
 import numpy as np
+import collections
 
 def find_common_numbers(x):
-    pass
+    test1 = collections.Counter(x)
+    return(test1)
 
 def pos_average(s):
     # your code
@@ -10,8 +12,9 @@ def pos_average(s):
     lists_of_lists = list(map(lambda x: list(x), string_split))
     numpy_array = np.array(lists_of_lists)
     int_array = numpy_array.astype(np.int16)
+    vallues = np.apply_along_axis(find_common_numbers, 0, int_array)
 
-    return(int_array)
+    return(vallues)
 
 test_str = "466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096"
 test1 = pos_average(test_str)
