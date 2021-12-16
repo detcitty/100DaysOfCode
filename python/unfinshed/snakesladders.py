568d4df0845cf6045545cc05966390f30e31617c
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-16 09:17:47 -0700

    12/16/21

---
+++ python/unfinshed/snakesladders.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/587136ba2eefcb92a9000027/train/python
+
+'''
+I will need to look at the board: There needs to be a snake and ladders mapping.
+'''
+class SnakesLadders():
+
+    def __init__(self):
+        # Code Here
+
+    def play(self, die1, die2):
+        # Code Here
