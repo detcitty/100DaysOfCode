7844aa6c84207361cdb6a62567383fc4b98ebc5a
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-19 13:34:58 -0700

    More updates

--- projecteuler/probl3m3.py
+++ projecteuler/probl3m3.py
@@ -21,8 +21,21 @@ def is_prime(n):
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
 def factorize_number(num):
-    pass
+    half_sqrt = math.sqrt(num)
+    test_num = num
+    diversor = 2
+    found_numbers = []
+    while (test_num > 1 ):
+        if (test_num % diversor == 0):
+            found_numbers.append(diversor)
+            test_num = test_num  / diversor
+        if(test_num % diversor != 0):
+            diversor += 1
+    print(found_numbers)
+    return(found_numbers[-1])
 
+test2 = factorize_number(20)
+print(test2)
 
 def start(number):    
     '''
@@ -41,5 +54,5 @@ def start(number):
 
     #map(lambda x, y: x(y), half_way, prime)
 
-test1 = start(600851475143)
-print(test1)
+#test1 = start(600851475143)
+#print(test1)
