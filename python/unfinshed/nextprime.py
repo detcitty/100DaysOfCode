8ac8c5d8d60bbf267ad247d240b823ea8a2b94e2
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-21 10:24:03 -0700

    Making updates

--- python/unfinshed/nextprime.py
+++ python/unfinshed/nextprime.py
@@ -1,4 +1,21 @@
-# What was the name of the website to test math problems.
+# https://www.codewars.com/kata/58e230e5e24dde0996000070/train/python
 
-def find_next_prime():
-    pass
+'''
+What was the name of the website to test math problems.
+
+Project Euler
+'''
+
+def next_prime(n):
+    # have fun ^.^
+    potential_prime = n+1
+    found_prime = False
+    while (found_prime == False):
+        all_indices = []
+        for i in range(1, n+1):
+            if n % i == 0:
+                all_indices.append(i)
+    return(all_indices)
+
+test1 = next_prime(14)
+print(test1)
