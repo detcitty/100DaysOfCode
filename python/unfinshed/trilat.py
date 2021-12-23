5a5b9111d0fd81b2ce9f029ba4a8774e88ca60e5
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-23 13:18:36 -0700

    12/23/21

--- python/unfinshed/trilat.py
+++ python/unfinshed/trilat.py
@@ -1,5 +1,10 @@
 # https://www.codewars.com/kata/57cebf1472f98327760003cd/train/python
 
+'''
+What does this do again? 
+Should I try to solve these problems again?`
+'''
+
 def count_col_triang(input_):
     # your code here
     colors = list(map(lambda x: x[-1]), input_)
