b41d1f79306570c172e9c87df45c43063d09b998
Devin Etcitty <devin.etcitty@gmail.com>
2021-12-24 15:53:39 -0700

    Help

--- python/unfinshed/numdivisions.py
+++ python/unfinshed/numdivisions.py
@@ -1,4 +1,15 @@
 # https://www.codewars.com/kata/5913152be0b295cf99000001/train/python
 
 def divisions(n, divisor):
-    pass
+    num = n
+    count = 0
+    while (num > 1):
+        if (num % divisor == 0) :
+            num = (num / divisor)
+            count += 1
+        else :
+            num = 1
+    return(count)
+
+test1 = divisions(9999, 3)
+print(test1)
