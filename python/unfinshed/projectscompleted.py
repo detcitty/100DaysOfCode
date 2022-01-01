1bfd2242547d3613c22e1b32a10758975bd68cc6
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-01 09:10:10 -0700

    COmpleted

--- python/unfinshed/projectscompleted.py
+++ python/unfinshed/projectscompleted.py
@@ -1,4 +1,20 @@
 # https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/python
 
+'''
+RULES: 
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+'''
+
 def final_grade(exam, projects):
-    return # final grade
+
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0 
