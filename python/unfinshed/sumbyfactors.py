3f103d36f614286b1472e9bcfc9ba8140076e3d3
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-03 09:29:04 -0700

    1.03.22

---
+++ python/unfinshed/sumbyfactors.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/54d496788776e49e6b00052f/train/python
+'''
+Given an array of positive or negative integers
+
+I= [i1,..,in]
+
+you have to produce a sorted array P of the form
+
+[ [p, sum of all ij of I for which p is a prime factor (p positive) of ij] ...]
+
+P will be sorted by increasing order of the prime numbers. The final result has to be given as a string in Java, C#, C, C++ and as an array of arrays in other languages.
+'''
+def sum_for_list(lst):
+    pass
