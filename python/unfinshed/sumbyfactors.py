bb6fcba1f5e368e0fc37660575924759111344c5
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-03 10:07:58 -0700

    Updates

--- python/unfinshed/sumbyfactors.py
+++ python/unfinshed/sumbyfactors.py
@@ -1,4 +1,6 @@
 # https://www.codewars.com/kata/54d496788776e49e6b00052f/train/python
+import numpy as np
+
 '''
 Given an array of positive or negative integers
 
@@ -11,4 +13,6 @@ you have to produce a sorted array P of the form
 P will be sorted by increasing order of the prime numbers. The final result has to be given as a string in Java, C#, C, C++ and as an array of arrays in other languages.
 '''
 def sum_for_list(lst):
+    
+    npl = np.array(lst)
     pass
