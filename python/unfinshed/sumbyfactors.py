12cc6f722e408b54820ab37e27fe4e6ac623f514
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-03 11:12:23 -0700

    Using sympy package

--- python/unfinshed/sumbyfactors.py
+++ python/unfinshed/sumbyfactors.py
@@ -1,5 +1,6 @@
 # https://www.codewars.com/kata/54d496788776e49e6b00052f/train/python
 import numpy as np
+import sympy
 
 '''
 Given an array of positive or negative integers
@@ -13,6 +14,10 @@ you have to produce a sorted array P of the form
 P will be sorted by increasing order of the prime numbers. The final result has to be given as a string in Java, C#, C, C++ and as an array of arrays in other languages.
 '''
 def sum_for_list(lst):
-    
+
     npl = np.array(lst)
+    values = list(map(lambda x: list(sympy.sieve.primerange(1,x)), lst))
+    print(values)
     pass
+a = [12, 15]
+test1 = sum_for_list(a)
