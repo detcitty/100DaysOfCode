91fddf612c8550f88789966c0d1113bbafd12e81
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-04 09:14:47 -0700

    Helping files

---
+++ python/unfinshed/incremeter.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/590e03aef55cab099a0002e8/train/python
+
+def incrementer(nums):
+    # your code here
+    new_sum = []
+    for index, value in enumerate(nums):
+        num_sum.append(index+1+value)
+    return(new_sum)
