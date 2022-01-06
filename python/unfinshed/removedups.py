3ae86db95645d8871b635fc0f8322efb297423a6
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-06 07:34:37 -0700

    Updating

--- python/unfinshed/removedups.py
+++ python/unfinshed/removedups.py
@@ -1,6 +1,8 @@
 # https://www.codewars.com/kata/5ba38ba180824a86850000f7/train/python
+import numpy as np
 
-def solve(arr): 
+def solve(arr):
+    print(np.unique(arr, return_index=True))
     return(list(set(arr)))
 
 test1 = solve([3,4,4,3,6,3])
