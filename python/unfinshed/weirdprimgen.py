bbe4a828227a602411b4d4db2e3680fce478daa3
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-08 07:50:39 -0700

    Move updates

---
+++ python/unfinshed/weirdprimgen.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/562b384167350ac93b00010c/train/python
+
+def count_ones(n):
+    # your code
+
+def max_pn(n):
+    # your code
+
+def an_over_average(n):
+    # your code
+
