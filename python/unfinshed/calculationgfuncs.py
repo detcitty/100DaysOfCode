49a359b91befc8849c6bc7e5f9374321250ae161
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-10 12:06:55 -0700

    1.10.22

---
+++ python/unfinshed/calculationgfuncs.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+
+def zero(): #your code here
+def one(): #your code here
+def two(): #your code here
+def three(): #your code here
+def four(): #your code here
+def five(): #your code here
+def six(): #your code here
+def seven(): #your code here
+def eight(): #your code here
+def nine(): #your code here
+
+def plus(): #your code here
+def minus(): #your code here
+def times(): #your code here
+def divided_by(): #your code here
