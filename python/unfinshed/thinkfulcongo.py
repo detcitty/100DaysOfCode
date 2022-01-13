9cbf61dc4f1bbfd8c8a806d73600ec22b392b89a
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-13 09:59:24 -0700

    1.13.22

---
+++ python/unfinshed/thinkfulcongo.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/5862e7c63f8628a126000e18/train/python
+
+
+'''
+Write a function box_capacity() that figures out how 
+many crates you can store in a given warehouse. The 
+function should take three arguments: the length, width, 
+and height of your warehouse (in feet) and should return an 
+integer representing the number of boxes you can store in that space.
+'''
+
+def box_capacity(length, width, height):
+    '''
+    For example: a warehouse 32 feet long, 64 feet wide, 
+    and 16 feet high can hold 13,824 boxes because you 
+    can fit 24 boxes across, 48 boxes deep, and 12 boxes high, 
+    so box_capacity(32, 64, 16) should return 13824.
+    '''
+    # Your code here.
