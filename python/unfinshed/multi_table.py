912fd87e9439e54896c8dd368d924a9dd312acd5
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-17 15:29:36 -0700

    Updates

--- python/unfinshed/multi_table.py
+++ python/unfinshed/multi_table.py
@@ -1,5 +1,9 @@
 # https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/python
-
+'''
+What does this do? 
+How do I try this value?
+What about multiplying and thinking of the fuctions in general?
+'''
 def multi_table(number):
     num_dic = {}
 
