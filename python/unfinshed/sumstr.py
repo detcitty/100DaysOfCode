29e722b58a17902e0e68b01a2860ee12c36f2377
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-17 15:37:09 -0700

    Other dates

--- python/unfinshed/sumstr.py
+++ python/unfinshed/sumstr.py
@@ -1,5 +1,8 @@
 # https://www.codewars.com/kata/5966e33c4e686b508700002d/train/python
 
+'''
+What does this even do?
+'''
 def sum_str(a, b):
     # happy coding !
     int_a = 0 if not a else int(a)
