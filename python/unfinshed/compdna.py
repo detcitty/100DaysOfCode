23912aa313a63b044b60a64143fef3dbba569be8
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-19 10:33:29 -0700

    1.19.22

---
+++ python/unfinshed/compdna.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
+
+def DNA_strand(dna):
+    # code here
+    dna_map = {
+        'A' : 'C',
+        'T' : 'G',
+        'C' : 'A',
+        'G' : 'T'
+    }
