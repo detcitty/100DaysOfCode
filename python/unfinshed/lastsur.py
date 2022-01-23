78a384d3a97a5c646b90de68efa207fa108d853b
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-23 07:20:43 -0700

    Other stuff

--- python/unfinshed/lastsur.py
+++ python/unfinshed/lastsur.py
@@ -1,5 +1,10 @@
 # https://www.codewars.com/kata/609eee71109f860006c377d1/train/python
 
+'''
+What does this do?
+What about doing Leetcode?
+'''
+
 def last_survivor(letters, coords): 
     # your code here
     values = list(map(lambda x, y: list(x).pop(y), letters, coords))
