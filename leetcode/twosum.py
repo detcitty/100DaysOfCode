84857da19f727bde787b2e4ab67ef1bdcb223033
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-23 17:04:42 -0700

    Other stuff

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -1,5 +1,13 @@
 # https://leetcode.com/problems/two-sum/
 
+'''
+CONSTRAINTS:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+'''
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         pass
