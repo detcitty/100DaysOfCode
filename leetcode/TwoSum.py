0bf2c3885bdc3f796e36ecdbc3f974e7032d7d8f
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-24 12:13:29 -0700

    Rename

---
+++ leetcode/TwoSum.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # List[int]  == [] of size int?
+        # Or is it an indeterminate amount of sizes
