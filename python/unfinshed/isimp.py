db65e9f3f8f5ffce786f42bad476eb45417683a1
Devin Etcitty <devin.etcitty@gmail.com>
2022-01-29 12:25:34 -0700

    1.29.22

--- python/unfinshed/isimp.py
+++ python/unfinshed/isimp.py
@@ -1,7 +1,10 @@
 # https://www.codewars.com/kata/556b85b433fb5e899200003f/train/python
+import re
+imprt numpy as np
 
 def mixed_fraction(s):
     num, denom = split("/")
     whole_ = int(int(num) / int(denom))
     remainder_ = int(num) % int(denom))
-    return None
+    test = np.array(whole_ / remainder)
+    return(test)
