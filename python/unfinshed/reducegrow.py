a8a804f26f40394fefc6f18976a22f66367c53bc
Devin Etcitty <devin.etcitty@gmail.com>
2022-02-09 13:21:50 -0700

    2.9.22

---
+++ python/unfinshed/reducegrow.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/57f780909f7e8e3183000078/train/python
+
+def grow(arr):
+    '''
+    Given a non-empty array of integers, 
+    return the result of multiplying the values 
+    together in order. Example:
+    '''
+    pass
