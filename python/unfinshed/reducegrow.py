9677693afdf029684108039df7b6c03598656d38
Devin Etcitty <devin.etcitty@gmail.com>
2022-02-09 13:23:28 -0700

    Other stuff

--- python/unfinshed/reducegrow.py
+++ python/unfinshed/reducegrow.py
@@ -1,9 +1,9 @@
 # https://www.codewars.com/kata/57f780909f7e8e3183000078/train/python
-
+import numpy as np
 def grow(arr):
     '''
     Given a non-empty array of integers, 
     return the result of multiplying the values 
     together in order. Example:
     '''
-    pass
+    return(np.multiply(arr))
