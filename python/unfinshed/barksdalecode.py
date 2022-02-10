16925abc126240bc230b6ec8ff0927a75de24c24
Devin Etcitty <devin.etcitty@gmail.com>
2022-02-10 13:26:49 -0700

    Other stuff

--- python/unfinshed/barksdalecode.py
+++ python/unfinshed/barksdalecode.py
@@ -2,9 +2,11 @@
 
 def decode(strng):
     #your code here
-    orignal = [1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, '']
-    shifted = [9, 8, 7, 6, 0, 4, 3, 2, 1, '', 5, '']
-    combinded = list(zip(orignal, shifted))
-    print(combinded)
+    orignal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    shifted = [9, 8, 7, 6, 0, 4, 3, 2, 1, 5]
+    combinded = dict(list(zip(orignal, shifted)))
+    str_nums = list(strng)
+    nums = list(map(lambda x: int(x), str_nums))
+    print(nums)
 
 print(decode('test'))
