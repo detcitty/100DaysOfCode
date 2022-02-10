f276c0423c27d4f4ad48fa9059cda095440c89d5
Devin Etcitty <devin.etcitty@gmail.com>
2022-02-10 13:16:08 -0700

    Other stuff

---
+++ python/unfinshed/barksdalecode.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/573d498eb90ccf20a000002a/train/python
+
+def decode(strng):
+    #your code here
+    orignal = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['', 0, '']]
+    shifted = [[]]
