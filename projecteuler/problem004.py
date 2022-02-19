a130ec9c58638157c290da96084b0deb0b47bfeb
Devin Etcitty <devin.etcitty@utah.edu>
2022-02-19 15:01:58 -0700

    today

--- projecteuler/problem004.py
+++ projecteuler/problem004.py
@@ -5,7 +5,7 @@ https://projecteuler.net/problem=4
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
-
+# what do i do?
 999*999 = 998001
 999*998 = 997002
 ..
