da475c68cbc9e9ac0499727e7d41a16d28c407ec
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-07 10:04:08 -0800

    3.7.22:

---
+++ python/unfinshed/reversewords.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/python
+
+def reverse_words(text):
+    #go for it
+    new_str = ''
+    list_text = list(text)
+
+    while len(text) > 0:
+        new_str += text.pop(-1)
+
+    return(new_str)
