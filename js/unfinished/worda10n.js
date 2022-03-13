6092ccabfab2801f10254b6c3e3c69f7a6c75fa2
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-13 13:37:54 -0700

    3.12.22

---
+++ js/unfinished/worda10n.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5375f921003bf62192000746/train/javascript
+
+function abbreviate(string) {
+  // ...
+}
--- python/unfinshed/validdate.py
+++ python/unfinshed/validdate.py
@@ -1,3 +1,14 @@
 # https://www.codewars.com/kata/548db0bd1df5bbf29b0000b7/train/python
+# Merged files
+# ./python/2020/05-May/2020-05-03/validDate.py
+import re
 
 valid_date = r'[01][01-31]|[02][01-28]|[03][01-30]'
+
+pattern = "(\[\d\d-\d\d\])"
+boject = re.search(pattern, "ignored [08-11] ignored") 
+print(boject.groups())
+if boject:
+    print(boject.expand("-"))
+
+#valid_date = 
