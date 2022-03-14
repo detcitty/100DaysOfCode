75d01def8370e494ea5e9894326eb301417e7c3b
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-13 20:04:37 -0700

    More docs

--- python/unfinshed/validdate.py
+++ python/unfinshed/validdate.py
@@ -1,6 +1,10 @@
 # https://www.codewars.com/kata/548db0bd1df5bbf29b0000b7/train/python
 # Merged files
 # ./python/2020/05-May/2020-05-03/validDate.py
+'''
+What does this return?
+A value or do I just need to come up with the regex?
+'''
 import re
 
 valid_date = r'[01][01-31]|[02][01-28]|[03][01-30]'
