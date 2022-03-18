2f31164795c77173d3764b94c8fe248c82ac9790
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-18 12:04:42 -0600

    Updating information

--- python/unfinshed/bouncing_balls.py
+++ python/unfinshed/bouncing_balls.py
@@ -1,5 +1,25 @@
 # https://www.codewars.com/kata/5544c7a5cb454edb3c000047/train/python
 
+'''
+  // your code here
+  // How would I make this an object-oreinted?
+
+  /*
+  drop() -> calculateHeight
+  drop() -> calculateHeight
+  
+  think about the direction too.
+
+  Drops 1 foot
+  Bounces 0.66 foot
+  drops 0.66 foot
+  bounces 0.44 foot
+  drops 0.44 foot
+  bounces 0.2962 foot
+
+  */
+'''
+
 class NumList:
     def __init__(self):
         self.__list = [] 
