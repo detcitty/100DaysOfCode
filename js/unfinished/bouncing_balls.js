881a10c082238b65de0aef90632dc7d61217e335
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-18 09:07:14 -0600

    Update

--- js/unfinished/bouncing_balls.js
+++ js/unfinished/bouncing_balls.js
@@ -2,4 +2,20 @@
 
 function bouncingBall(h,  bounce,  window) {
   // your code here
+  // How would I make this an object-oreinted?
+
+  /*
+  drop() -> calculateHeight
+  drop() -> calculateHeight
+  
+  think about the direction too.
+
+  Drops 1 foot
+  Bounces 0.66 foot
+  drops 0.66 foot
+  bounces 0.44 foot
+  drops 0.44 foot
+  bounces 0.2962 foot
+
+  */
 }
