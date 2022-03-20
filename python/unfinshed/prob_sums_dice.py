60dd26ea1922f98d19b71683dd47d757c4b566f2
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-20 17:39:56 -0600

    Help desk

---
+++ python/unfinshed/prob_sums_dice.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56f78a42f749ba513b00037f/train/python
+
+def rolldice_sum_prob(sum_, dice_amount):
+    # your code here
+    return prob             
+    # prob = probability of having the sum value
+    # when rolling dice
