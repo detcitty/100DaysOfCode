6509763e8440b2639ff3b0b75649a2b6a4bc68eb
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-25 12:38:58 -0600

    what is a higher order function?

---
+++ js/unfinished/PyramidSlideDown.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/551f23362ff852e2ab000037/train/python
+'''
+Here comes the task...
+Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid. 
+As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
+
+Your task is to write a function that takes a pyramid representation as argument and returns its' largest 'slide down'. For example:
+'''
+
+def longest_slide_down(pyramid):
+    # TODO: write some code...
+    pass
