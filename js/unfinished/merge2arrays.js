bb402f7bb10e637d2d9ea1559e29804c6918aee8
Devin Etcitty <devin.etcitty@gmail.com>
2022-03-25 12:32:53 -0600

    Questions

--- js/unfinished/merge2arrays.js
+++ js/unfinished/merge2arrays.js
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/5899642f6e1b25935d000161/train/javascript
-import structuredClone from '@ungap/structured-clone';
+//import structuredClone from '@ungap/structured-clone';
 
+/*
+What are some of the issues to work on?
+I want to format the code.
+*/
 function mergeArrays(arr1, arr2) {
     let array3 = arr1.concat(arr2);
     let deep_clone = structuredClone(array3)
