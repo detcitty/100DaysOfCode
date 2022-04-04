273da7b75f40eec028a46d0e1bef5f551b684d09
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-04 08:55:38 -0600

    Updating

--- js/unfinished/findOddInt.js
+++ js/unfinished/findOddInt.js
@@ -1,11 +1,13 @@
 //https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
 
 //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-const onlyUnique = (value, index, self) => { return self.indexOf(value) === index };
+const onlyUnique = (value, index, self) => self.indexOf(value) === index;
 
 function findOdd(A) {
   //happy coding!
   let unique_values = A.filter(onlyUnique);
+  let dict = {};
+  unique_values.sort()
   return(unique_values);
 }
 let test1 = findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);
