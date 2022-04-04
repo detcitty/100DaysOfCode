1db6f8aa97cd1f3e69c6d6bbadfd25daa24f44ab
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-04 08:40:55 -0600

    Fat arrow test

--- js/unfinished/findOddInt.js
+++ js/unfinished/findOddInt.js
@@ -1,9 +1,7 @@
 //https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
 
-function onlyUnique(value, index, self) {
-    //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-    return self.indexOf(value) === index;
-  }
+//https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+const onlyUnique = (value, index, self) => { self.indexOf(value) === index };
 
 function findOdd(A) {
   //happy coding!
