2be01909a6178507bf7e76c45da8b5481a2acde2
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-17 10:56:21 -0600

    Updating

--- js/unfinished/countChar.js
+++ js/unfinished/countChar.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
-
+///https://dev.to/j471n/javascript-cheat-sheet-you-needed-2id
 function count (string) {  
     // The function code should be here
-    string.
+    let words_split = string.split('');
      return {};
   }
