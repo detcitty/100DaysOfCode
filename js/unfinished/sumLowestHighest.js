67e0883455db2a09475fe467bff5cdfa5cf6736e
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-20 07:59:01 -0600

    Chaining

---
+++ js/unfinished/sumLowestHighest.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/javascript
+
+function sumArray(array) {
+    arr.reduce((a,b) => a + b, 0)
+    return( Math.sum(array) - (Math.max(array) + Math.min(array)))
+}
