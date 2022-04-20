b3dbb951baafb6ef3d92a8da953836e2acd43c48
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-20 08:04:11 -0600

    updating

--- js/unfinished/sumLowestHighest.js
+++ js/unfinished/sumLowestHighest.js
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/javascript
 
 function sumArray(array) {
-    arr.reduce((a,b) => a + b, 0)
-    return( Math.sum(array) - (Math.max(array) + Math.min(array)))
+    
+  const sumWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+  return sumWithInitial - (Math.max(array) + Math.min(array));
 }
