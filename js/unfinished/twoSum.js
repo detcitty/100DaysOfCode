fe2c83de681afec0e0c187c8b7caa05472849538
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-21 08:32:22 -0600

    more updating stuff

---
+++ js/unfinished/twoSum.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+
+function twoSum(numbers, target) {
+    // ...
+    for(let i = 0; i < numbers.length; i++) {
+
+        for (let j = 0; j < numbers.length; j++) {
+            if (i == j) continue
+            else {
+                
+            }
+        }
+    }
+}
