850d5dea7f742d0572e9af8b29730149776d4343
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-21 08:36:20 -0600

    More work

--- js/unfinished/twoSum.js
+++ js/unfinished/twoSum.js
@@ -2,13 +2,22 @@
 
 function twoSum(numbers, target) {
     // ...
-    for(let i = 0; i < numbers.length; i++) {
+    var index1 = 0;
+    var index2 = 0;
+    for (let i = 0; i < numbers.length; i++) {
 
         for (let j = 0; j < numbers.length; j++) {
+            values = numbers[i] + numbers[j];
+
             if (i == j) continue
             else {
-                
+                if (values == target) {
+                    index1 = i;
+                    index2 = j;
+                    break
+                }
             }
         }
     }
+    return ((index1, index2))
 }
