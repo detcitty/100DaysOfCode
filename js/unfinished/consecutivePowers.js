3b517d4440a3093514d9c496962bf8dd9131ed6e
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-22 07:20:00 -0600

    JS stuff

---
+++ js/unfinished/consecutivePowers.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/javascript
+
+function sumDigPow(a, b) {
+  // Your code here
+  /*
+  89 = 8^1  + 9^2
+  */
+  for (var i = a; i <= b; i++) {
+    var nums = String(i)
+      .split("")
+      .map((str) => Number(str));
+  }
+}
