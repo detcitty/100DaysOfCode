ef9fb6df183ed325c6c5d41842b953207340caaa
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-24 08:34:48 -0700

    help

--- js/unfinished/countChar.js
+++ js/unfinished/countChar.js
@@ -4,5 +4,8 @@ function count (string) {
     // The function code should be here
     let words_split = string.split('');
     let words_size = words_split.length;
+    for (v in words_split) {
+        let vare = v[1];
+    }
      return {};
   }
