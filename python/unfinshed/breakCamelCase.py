26832edf41728fa86b21e459450ed5b9f7b304a4
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-26 13:16:58 -0600

    Helping stuff

--- python/unfinshed/breakCamelCase.py
+++ python/unfinshed/breakCamelCase.py
@@ -2,18 +2,20 @@
 
 def solution(s):
     array_letters = list(s)
-    
-    index = 0
+    i = 0
+
     found_end = False
     while not found_end:
-        if array_letters[index].isupper():
-            array_letters.insert(index, " ")
-            index += 2
-        elif index == len(array_letters):
+        letter = array_letters[i]
+        if letter.isupper():
+            array_letters.insert(i, " ")
+            i += 2
+        elif i == len(array_letters)-1:
             found_end = True
         else:
-            index += 1
+            i += 1
             
     return ''.join(array_letters)
 
 test1 = solution("helloWorld")
+print(test1)
