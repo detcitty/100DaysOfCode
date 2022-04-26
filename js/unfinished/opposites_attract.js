086f2faca7c3aef6380910e70423cf7f56e15aec
Devin Etcitty <devin.etcitty@gmail.com>
2022-04-26 12:53:04 -0600

    daily coding

---
+++ js/unfinished/opposites_attract.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  var test = [flower1, flower2].forEach((e = e % 2));
+  return test.every(Boolean);
+}
---
+++ python/unfinshed/breakCamelCase.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+
+def solution(s):
+    pass
