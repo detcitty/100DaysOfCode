0f97f0418b39031701f88bba62e20775445bb32b
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-03 10:57:19 -0600

    Getting stuff

--- python/unfinshed/factorial_test.py
+++ python/unfinshed/factorial_test.py
@@ -1,4 +1,8 @@
 # https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc/train/python
 
 def factorial(n):
-    pass
+    try:
+        some_code_that_may_raise_our_value_error()
+
+    except ValueError as err:
+        print(err.args)
