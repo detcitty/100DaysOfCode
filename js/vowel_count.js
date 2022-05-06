ce882618906a3613b730e391eefc13519c1b166e
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-06 11:01:57 -0600

    Updating info

---
+++ js/vowel_count.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+function getCount(str) {
+  let vowelsCount = 0;
+  
+  // enter your magic here
+  
+  return vowelsCount;
+}
