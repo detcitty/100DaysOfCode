9807a313d8c737d81420d5bce7380c3a19178587
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-08 21:58:20 -0600

    Updating

--- js/unfinished/digitsExplosion.js
+++ js/unfinished/digitsExplosion.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/585b1fafe08bae9988000314/train/javascript
 
 function explode(s) {
-  s_split = s.split('');
-  return ();
+  var s_split = s.split('');
+  var new_match = s_split.map(x => (x) );
+  return (new_match);
