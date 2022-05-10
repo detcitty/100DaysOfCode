f2472387f8efb76685cbbf3a991ac00343819a40
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-10 12:06:34 -0600

    updating

--- js/unfinished/transportation_vacation.js
+++ js/unfinished/transportation_vacation.js
@@ -2,4 +2,16 @@
 
 function rentalCarCost(d) {
   // Your solution here
+  var total_charges = 40 * d;
+  var discount = 0;
+  if (d >= 7) {
+    discount = 50;
+  }
+  else if (d >= 3) {
+    discount = 20;
+  }
+  else {
+    discount = 0;
+  }
+  return(total_charges - discount)
 }
