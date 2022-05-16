6627d85229edc68063b0ada90a26a51bc44d321d
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-16 08:31:03 -0600

    update

--- js/unfinished/max_length_diff.js
+++ js/unfinished/max_length_diff.js
@@ -1,5 +1,17 @@
 // https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
 
 function mxdiflg(a1, a2) {
-    // your code
+  // your code
+  var max_int = 0;
+  if (a1.length == 0 || a2.length == 0) {
+    max_int = -1;
+  } else {
+    for (var j = 0; j < a2.length; j++) {
+      for (var i = 0; i < a1.length; i++) {
+        var diff = Math.abs(a2[j].length - a1[i].length);
+        max_int = diff > max_int ? diff : max_int;
+      }
+    }
+  }
+  return max_int;
 }
