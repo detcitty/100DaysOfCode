75f9433828cf3a89a376f1e66771d81353e5bc9d
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-17 16:09:45 -0600

    JS daily

--- js/unfinished/are_they_the_same.js
+++ js/unfinished/are_they_the_same.js
@@ -2,4 +2,17 @@
 
 function comp(array1, array2){
     //your code here
+    var squared = array1.map(x => Math.pow(x, 2));
+    squared.sort();
+    var sqrt = array2.map(x => Math.sqrt(x));
+    sqrt.sort();
+    var is_same = true;
+    for (var i = 0; i < array1.length; i++){
+        if(squared[i] == array2[i]) {
+            continue;
+        } else {
+            is_same = false;
+        }
+    }
+    return(is_same)
   }
