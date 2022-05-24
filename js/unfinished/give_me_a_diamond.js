28000fc66c92121ad89f2ba692f19a2f78b6571c
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-24 12:04:04 -0600

    workign

--- js/unfinished/give_me_a_diamond.js
+++ js/unfinished/give_me_a_diamond.js
@@ -1,14 +1,14 @@
 // https://www.codewars.com/kata/5503013e34137eeeaa001648/train/javascript
 
 function diamond(n) {
-  var str_value = '';
+  var final_return_value = "";
   if (n % 2 == 0 || n < 0) {
-    str_value = null;
+    final_return_value = null;
   } else {
-
-      for (var i = 1; i <= n ; i+=2) {
-          str_value += '*'.repeat(i) +'\n'; 
-      }
+    for (var i = 1; i <= n; i += 2) {
+      var str_value = "*".repeat(i) + "\n";
+      var reverse = str_value.split("").reverse().join("");
+    }
   }
-  return str_value;
+  return final_return_value;
 }
