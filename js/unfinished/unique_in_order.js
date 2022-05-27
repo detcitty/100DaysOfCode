4a1cc0315bc25ae7c33298675bd7e36bfaa32134
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-27 08:33:03 -0600

    help

--- js/unfinished/unique_in_order.js
+++ js/unfinished/unique_in_order.js
@@ -2,4 +2,13 @@
 
 var uniqueInOrder=function(iterable){
   //your code here - remember iterable can be a string or an array
+  // convert  string to array
+  var convert_to_array = null;
+  if (!Array.isArray(iterable) ) {
+      convert_to_array = iterable.split('');
+  }
+  else {
+      convert_to_array = iterable;
+  }
+
 }
