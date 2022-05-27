3db188d492560b92f8a7bbdd1b92862955504ecf
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-27 08:26:15 -0600

    today

---
+++ js/unfinished/unique_in_order.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+
+var uniqueInOrder=function(iterable){
+  //your code here - remember iterable can be a string or an array
+}
