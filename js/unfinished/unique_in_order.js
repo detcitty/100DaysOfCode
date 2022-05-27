db06aad0c8490afd75c30bc684179e75cd3839df
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-27 08:50:36 -0600

    maybe finished

--- js/unfinished/unique_in_order.js
+++ js/unfinished/unique_in_order.js
@@ -1,14 +1,16 @@
 //https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
 
-var uniqueInOrder=function(iterable){
+function onlyUnique(currentValue, index, arr) {
+  return currentValue != arr[index - 1];
+}
+
+var uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
   // convert  string to array
   var convert_to_array = null;
-  if (!Array.isArray(iterable) ) {
-      convert_to_array = iterable.split('');
+  if (!Array.isArray(iterable)) {
+    convert_to_array = iterable.split("");
+  } else {
+    convert_to_array = iterable;
   }
-  else {
-      convert_to_array = iterable;
-  }
-
-}
+};
