9c15422aa0de23df5220418f20a91b58a043bfb7
Devin Etcitty <devin.etcitty@gmail.com>
2022-05-29 21:39:23 -0600

    finished

--- js/unfinished/super_easy.js
+++ js/unfinished/super_easy.js
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
 
-function problem(x){
-  //your code here
-  return((x*50 + 6))
+function problem(x) {
+    //your code here
+    return ((typeof x === 'string' || x instanceof String) ? "Error" : (x * 50 + 6));
 }
