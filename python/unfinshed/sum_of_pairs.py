0cf072af6e055cf8517ba90e2304f552a04c5c5d
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-01 11:26:18 -0600

    Directions

---
+++ python/unfinshed/sum_of_pairs.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/54d81488b981293527000c8f/train/python
+'''
+Sum of Pairs
+Given a list of integers and a single sum value, 
+return the first two values (parse from the left please) 
+in order of appearance that add up to form the sum.
+'''
+
+def sum_pairs(ints, s):
+    return
