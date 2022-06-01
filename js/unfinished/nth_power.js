550afbb33ad79ef712165982cf6faa37378f0fed
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-01 11:09:48 -0600

    WIP

--- js/unfinished/nth_power.js
+++ js/unfinished/nth_power.js
@@ -1,5 +1,13 @@
 // https://www.codewars.com/kata/57d814e4950d8489720008db/train/javascript
 
-function index(array, n){
+function index(array, n) {
   //your code here
+  var return_value = 0;
+  if (array.length > n) {
+    return_value = -1;
+  } else {
+    return_value = array[n] * array[n];
+  }
+
+  return return_value;
 }
