168ff0c166008e3342780029974b9abbc1157005
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-02 12:05:39 -0600

    help

--- python/unfinshed/sum_of_pairs.py
+++ python/unfinshed/sum_of_pairs.py
@@ -5,8 +5,11 @@ Given a list of integers and a single sum value,
 return the first two values (parse from the left please) 
 in order of appearance that add up to form the sum.
 '''
-import numpy as np
-
+#import numpy as np
+import statistics
 def sum_pairs(ints, s):
-    
+    mean_ints = statistics.mean(ints)
+    median_ints = statistics.median(ints)
+    mode_ints = statistics.mode(ints)
+    half_ints = [x / 2.0 for x in ints]
     return
