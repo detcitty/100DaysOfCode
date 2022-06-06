d15ae44c620d87fb03dfbc6a0b68ba9a57d474c2
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-06 08:05:44 -0600

    work

--- python/unfinshed/isograms.py
+++ python/unfinshed/isograms.py
@@ -3,5 +3,17 @@
 def is_isogram(string):
     #your code here
     letters = list(string)
+    letters.remove
+    not_unique = True
+    
+    for count, l in enumerate(letters):
+        
+        if (letters[count-1] == l):
+            not_unique = False
+            break
+        else:
+            continue
+        
+    return(not_unique)
     
     
