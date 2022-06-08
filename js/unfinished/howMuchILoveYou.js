89b26dafe67be008d5886c3cdc2e96d0e933e0f8
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-08 08:32:42 -0600

    JS

---
+++ js/unfinished/howMuchILoveYou.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/javascript
+
+function howMuchILoveYou(nbPetals) {
+  // your code
+
+  let words = new Map([
+    [1, "I love you"],
+    [2, "a little"],
+    [3, "a lot"],
+    [4, "passionately"],
+    [5, "madly"],
+    [6, "not at all"],
+  ]);
+}
