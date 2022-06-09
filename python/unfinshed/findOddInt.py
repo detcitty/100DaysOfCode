137a92c27c56c6e7ad26f42ce8dd894df0f5a3f6
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-09 10:49:16 -0600

    working

--- python/unfinshed/findOddInt.py
+++ python/unfinshed/findOddInt.py
@@ -1,4 +1,12 @@
 # https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
 
 def find_it(seq):
+    unique_nums = set(seq)
+    tracks = {}
+    
+    for i, e in enumerate(unique_nums):
+        tracks[e] = 0
+        
+    for x in seq:
+        
     return None
