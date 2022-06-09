200db182ae20c27f5fa6187f13e408351b8f6a57
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-09 10:58:25 -0600

    work

---
+++ js/getCharacterASCII.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+
+/*
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+For ASCII table, you can refer to http://www.asciitable.com/
+*/
+function getChar(c){
+  // ...
+}
