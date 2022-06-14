97d117f82a07472af1cfda9109fa4a259414d02e
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-14 09:17:32 -0600

    updating

---
+++ python/unfinshed/theCouponCode.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/539de388a540db7fec000642/train/python
+'''
+Your mission:
+Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+'''
+def check_coupon(entered_code, correct_code, current_date, expiration_date):
+    #Code here!
+    pass
