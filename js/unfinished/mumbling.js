f7b6595b9ca4570281b22b2b48fe2b5fcf903c20
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-20 19:00:09 -0600

    Help

---
+++ js/unfinished/mumbling.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+/*
+This time no story, no theory. The examples below show you how to write function accum:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+function accum(s) {
+	// your code
+}
