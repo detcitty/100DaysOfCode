a66cbb4be18781cfffbe0585cc846259b0211afe
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-23 07:51:39 -0600

    help

---
+++ js/unfinished/offspringSex.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56530b444e831334c0000020/train/javascript
+
+/*
+Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+*/
+function chromosomeCheck(sperm) {
+
+}
