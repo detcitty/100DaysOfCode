77ed296e49e303ec489ea89c382be3c692e1f797
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-23 07:56:51 -0600

    what

--- js/unfinished/offspringSex.js
+++ js/unfinished/offspringSex.js
@@ -4,5 +4,13 @@
 Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
 */
 function chromosomeCheck(sperm) {
+    var returnv = '';
+    if (sperm = 'XY') {
+        returnv = "Congratulations! You're going to have a son."
+    }
+    else {
+        returnv = "Congratulations! You're going to have a daughter."
 
+    }
+    return(returnv)
 }
