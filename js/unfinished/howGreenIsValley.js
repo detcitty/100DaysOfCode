2f1d34a5927a798dc7e953901b363bb84aae95f9
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-28 09:27:31 -0600

    directions

--- js/unfinished/howGreenIsValley.js
+++ js/unfinished/howGreenIsValley.js
@@ -1,5 +1,23 @@
 //https://www.codewars.com/kata/56e3cd1d93c3d940e50006a4/train/javascript
 
+
+/*
+
+
+    Input : an array of integers.
+
+    Output : this array, but sorted in such a way that there are two wings:
+
+    the left wing with numbers decreasing,
+
+    the right wing with numbers increasing.
+
+    the two wings have the same length. If the length of the array is odd the wings are around the bottom, if the length is even the bottom is considered to be part of the right wing.
+
+    each integer l of the left wing must be greater or equal to its counterpart r in the right wing, the difference l - r being as small as possible. In other words the right wing must be nearly as steep as the left wing.
+
+The function is make_valley or makeValley or make-valley.
+*/
 function makeValley(arr) {
     // Your code
 }
