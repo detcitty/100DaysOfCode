8ec5b74577e882f61a46c685090600b33563830c
Devin Etcitty <devin.etcitty@gmail.com>
2022-06-28 09:15:20 -0600

    Help

---
+++ python/unfinshed/titleCase.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/5202ef17a402dd033c000009/train/python
+'''
+
+First argument (required): 
+- the original string to be converted.
+Second argument (optional): 
+- space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+
+'''
+def title_case(title, minor_words=''):
+    pass
+    
