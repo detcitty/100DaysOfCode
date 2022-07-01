4649a93cb588c0a50c3e5ef60c7175024d989bbd
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-01 08:05:39 -0600

    working stuff

---
+++ python/unfinshed/SumWithoutHighestLower.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/python
+'''
+Task
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+'''
+def sum_array(arr):
+    #your code here
+    pass
