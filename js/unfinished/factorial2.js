c3c682e67cef9ca610ae86abcc409e5ab4f5b97c
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-02 09:35:09 -0600

    working doen

--- js/unfinished/factorial2.js
+++ js/unfinished/factorial2.js
@@ -5,6 +5,11 @@ Your task is to write function factorial.
 https://en.wikipedia.org/wiki/Factorial
 
 */
-function factorial(n){
+function factorial(n) {
   //your code here
+  var total = 1;
+  for (var i = n; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 }
