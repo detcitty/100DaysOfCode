c661b587d5f6ccb1b13e4a8406e8bb488e8f200f
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-02 09:33:12 -0600

    ?

---
+++ js/unfinished/factorial2.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
+/*
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+
+*/
+function factorial(n){
+  //your code here
+}
