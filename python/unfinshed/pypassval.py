3c1d37ca88e1fbf1206856d8dfe76a32efcca824
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-08 08:47:38 -0700

    python coding

--- python/unfinshed/pypassval.py
+++ python/unfinshed/pypassval.py
@@ -1,3 +1,11 @@
 # https://www.codewars.com/kata/52e1476c8147a7547a000811/train/python
+'''
+You need to write regex that will validate a password to make sure it meets the following criteria:
 
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a number
+Valid passwords will only be alphanumeric characters.
+'''
 regex = r'([A-Za-z0-9]{6,}[A-Za-z0-9]{6,}[A-Za-z0-9]{6,})(?<![^A-Za-z0-9])'
