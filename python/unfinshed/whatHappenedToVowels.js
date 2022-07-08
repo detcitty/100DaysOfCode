fa7f7e7dff761ab22cf26e2d9ffe5d33dfcf0d21
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-08 08:59:09 -0700

    oh the drama

---
+++ python/unfinshed/whatHappenedToVowels.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/596e7c6960de9525c9000155/train/javascript
+
+
+/*
+The Polish language is famous for its difficult to pronounce words with very few vowels. 
+For example: Pszczyna, a town in Southern Poland, or scczescie, which means happiness.
+
+Your task is to create a function that would generate similar words with very few vowels. 
+Specifically, all possible combinations from the given word, by preserving the consonants 
+in place while swapping the vowel(s) in the word with other vowels to create as many possible permutations.
+
+For example: output for generator (Pszczyna) would produce an array: 
+['pszczyna', 'pszczyne', 'pszczyni', 'pszczyno', 'pszczynu'].
+
+Vowels are: A, E, I, O, and U
+
+Input: string (length > 0), that consists of only letter characters, of which no more than three vowels.
+
+Output: alphabetically sorted array of words in lowercase, with all possible combinations of vowels.
+
+Don't forget to try to pronounce what you produce!
+*/
+
+
+function wordGenerator(str){
+  //your code here
+}
