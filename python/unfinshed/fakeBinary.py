f24ee381c6dc4ba4607b8179a9e7a6a7a002630a
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-09 12:20:12 -0700

    more testing

--- python/unfinshed/fakeBinary.py
+++ python/unfinshed/fakeBinary.py
@@ -1,3 +1,10 @@
 # https://www.codewars.com/kata/57eae65a4321032ce000002d/train/python
+'''
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+Return the resulting string.
+Note: input will never be an empty string
+
+'''
+
 def fake_bin(x):
     pass
