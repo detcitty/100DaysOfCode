c6d7e6a6c70162ed93304da3c2acfd9db197c55a
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-11 10:26:37 -0600

    help me

--- python/unfinshed/enumerableMagic3.py
+++ python/unfinshed/enumerableMagic3.py
@@ -1,5 +1,8 @@
 # https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/python
+'''
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
+'''
 def include(arr,item):
     # code
     pass
