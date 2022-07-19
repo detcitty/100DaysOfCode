99a133e3fd5a86d0de0b1a89b52bca94e9c5cf95
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-19 09:14:08 -0600

    helping them

--- python/unfinshed/reducegrow.py
+++ python/unfinshed/reducegrow.py
@@ -1,33 +1,13 @@
 # https://www.codewars.com/kata/57f780909f7e8e3183000078/train/python
-import numpy as np
+
+'''
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+'''
+import functools
+
+
 def grow(arr):
-    '''
-    Given a non-empty array of integers, 
-    return the result of multiplying the values 
-    together in order. Example:
-    '''
-    np_array = np.array(arr)
-    product = np.multiply(np_array)
-    print(product)
-    return()
-
-
-test1 = grow([1, 2, 3])
-print(test1)
-'''
-import codewars_test as test
-from solution import grow
-
-@test.describe("Fixed Tests")
-def fixed_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        tests = [
-            [6 , [1, 2, 3]],
-            [16, [4, 1, 1, 1, 4]],
-            [64, [2, 2, 2, 2, 2, 2]],
-        ]
-        
-        for exp, inp in tests:
-            test.assert_equals(grow(inp), exp)
-'''
+    return(functools.reduce(lambda x, y: x*y, arr))
