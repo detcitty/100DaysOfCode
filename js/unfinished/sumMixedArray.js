bc9278917835b32b764f6abe5d65a59b9ec98f81
Devin Etcitty <devin.etcitty@gmail.com>
2022-07-20 08:22:52 -0600

    work

--- js/unfinished/sumMixedArray.js
+++ js/unfinished/sumMixedArray.js
@@ -1,5 +1,11 @@
 // https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/javascript
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-function sumMix(x){
+Return your answer as a number.
 
+*/
+function sumMix(x) {
+  var s_new = x.map((x) => parseInt(x)).reduce((sum, number) => sum + number);
+  return(s_new)
 }
