aa225b876bf36f29cc4988ca0f9b45da5adce467
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-01 10:50:30 -0600

    Help

--- js/unfinished/larioMuigiPipeProblem.js
+++ js/unfinished/larioMuigiPipeProblem.js
@@ -2,21 +2,30 @@
 /*
 Issue
 
-Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+Looks like some hoodlum plumber and his brother has 
+been running around and damaging your stages again.
 
-The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+The pipes connecting your level's stages together need 
+to be fixed before you receive any more complaints.
 
-Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+Pipes list is correct when each pipe after the first index 
+is greater (+1) than the previous one, and that there is no duplicates.
 Task
 
-Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+Given the a list of numbers, return a fixed list so that the values increment 
+by 1 for each index from the minimum value up to the maximum value (both included).
 Example
 
 Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
 */
 
-
-function pipeFix(numbers){
-
+function pipeFix(numbers) {
+  var new_numbers = numbers;
+  for (var i = 0; i < new_numbers.length; i++) {
+    if (new_numbers[i + 1] - new_numbers[i] != 1) {
+      new_months.splice(i, 0, new_numbers[i] + 1);
+    }
+  }
+  return new_numbers;
 }
