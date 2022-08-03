43ea08d43b73f4ef8aeb2f0b77c5883630a7838d
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-03 11:54:55 -0600

    help

--- python/unfinshed/palindrome_strings.py
+++ python/unfinshed/palindrome_strings.py
@@ -21,11 +21,16 @@ def is_palindrome(string):
     new_string = string if isinstance(string, str) else str(string)
     is_even = True if len(new_string) % 2 == 0 else False
     middle_value = math.floor(len(new_string) / 2)
-    
+    found_palindrome = True
+    new_string_list = list(new_string)
     
     for i in range(0, middle_value):
         first_index = i 
         last_index = len(new_string) - i
-        if (new_string(first_index) == new_string(last_index))
+        if (new_string[first_index] == new_string[last_index]):
+            continue
+        else:
+            found_palindrome = False
+            break
     
-    
+    return(found_palindrome)
