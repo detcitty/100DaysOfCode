4748b93019af46427b62c743187aceec2c037ced
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-08 10:43:25 -0600

    Test

--- js/unfinished/greedIsGood.js
+++ js/unfinished/greedIsGood.js
@@ -23,6 +23,7 @@ Example scoring
  2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
 In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
 
+// Should I make this object oriented?
 
 */
 function score( dice ) {
---
+++ python/unfinshed/countCharsString.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/python
+'''
+def count(string):
+    # The function code should be here
+    return {}
+'''
+def count(string):
+    # The function code should be here
+    return {}
