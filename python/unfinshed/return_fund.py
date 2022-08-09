c965a2fea9e8a06ac92ca3a20e7c4cf2b84dafbd
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-09 11:11:21 -0600

    finished working stuff

--- python/unfinshed/return_fund.py
+++ python/unfinshed/return_fund.py
@@ -1,8 +1,45 @@
 # https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/python
+'''
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
-def add(a,b):
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+
+'''
+
+
+def add(a, b):
     # Your Code Here
-    
+    return(a + b)
+
+
+def multiply(a, b): return a * b
+
+
+def divide(a, b): return a / b
+
+
+def mod(a, b): return a % b
+
+
+def exponent(a, b): return a ** b
+
+
+def subt(a, b): return a - b
+
 # Make more functions. Refer to the description for function names.
 # The code will NOT WORK IF YOU USE names other than the ones
 # from the description
