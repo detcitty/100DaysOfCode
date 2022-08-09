b3a5ae8062b7220e725fec3b41b7ffe009a48d0b
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-09 11:07:02 -0600

    working stuff

---
+++ python/unfinshed/return_fund.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/python
+
+def add(a,b):
+    # Your Code Here
+    
+# Make more functions. Refer to the description for function names.
+# The code will NOT WORK IF YOU USE names other than the ones
+# from the description
