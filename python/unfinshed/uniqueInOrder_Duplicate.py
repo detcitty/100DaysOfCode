4b3d221b390b443baa91fb3ade68815b1ef44c12
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-19 12:58:14 -0600

    working stuff

--- python/unfinshed/uniqueInOrder_Duplicate.py
+++ python/unfinshed/uniqueInOrder_Duplicate.py
@@ -10,7 +10,11 @@ unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 unique_in_order([1,2,2,3,3])       == [1,2,3]
 '''
+
+
 def unique_in_order(iterable):\
-    # How do I make an iterable?
+        # How do I make an iterable?
     # I can do this.
+    values = list(iterable) if isinstance(iterable, list) else iterable
+    # check if a list
     pass
