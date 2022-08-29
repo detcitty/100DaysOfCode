c03e4369c82fad57a09e68a440976f03f4044677
Devin Etcitty <devin.etcitty@gmail.com>
2022-08-29 10:29:27 -0600

    more stuff to do

--- js/unfinished/consecutivePowers.js
+++ js/unfinished/consecutivePowers.js
@@ -1,6 +1,9 @@
 //https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/javascript
+
 /*
-The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+The number 89 is the first integer with more than one digit that fulfills the 
+property partially introduced in the title of this kata. What's the use of 
+saying "Eureka"? Because this sum gives the same number.
 
 In effect: 89 = 8^1 + 9^2
 
@@ -8,7 +11,9 @@ The next number in having this property is 135.
 
 See this property again: 135 = 1^1 + 3^2 + 5^3
 
-We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+We need a function to collect these numbers, that may receive two integers a, b 
+that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers 
+in the range that fulfills the property described above.
 
 Let's see some cases (input -> output):
 
@@ -16,13 +21,15 @@ Let's see some cases (input -> output):
 
 1, 100 -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
 
-If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+If there are no numbers of this kind in the range [a, b] the function should 
+output an empty list.
 
 90, 100 --> []
 
 Enjoy it!!
-
 */
+
+
 function sumDigPow(a, b) {
   // Your code here
   /*
