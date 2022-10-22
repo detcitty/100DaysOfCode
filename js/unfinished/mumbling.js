7c20a925d2d3d8818c097a4e35120facfd383e0d
Devin Etcitty <devin.etcitty@gmail.com>
2022-10-21 23:06:08 -0600

    work

--- js/unfinished/mumbling.js
+++ js/unfinished/mumbling.js
@@ -5,6 +5,8 @@ This time no story, no theory. The examples below show you how to write function
 accum("abcd") -> "A-Bb-Ccc-Dddd"
 accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
+this is correct? 
+git
 */
 function accum(s) {
 	// your code
