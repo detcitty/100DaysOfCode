e12cf2a6dbfb2904f9d1a30f69cd764cceae411f
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-11-01 17:30:26 -0600

    stuff

--- python/unfinshed/multiplication_table.py
+++ python/unfinshed/multiplication_table.py
@@ -1,4 +1,5 @@
 # https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/python
+# https://stackoverflow.com/questions/18072759/comprehension-on-a-nested-iterables
 '''
 Your task, is to create NxN multiplication table, of size provided in parameter.
 
@@ -13,5 +14,8 @@ for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 '''
 def multiplication_table(size):
     indices = [x for x in range(1, size+1)]
-    return(list(map(lambda x: x * indices, indices)))
-    #return # good luck
+    new_list = [y*x for x in indices for y in indices]
+    #new_list = [y*x for x in indices for y in indices]
+
+    return(new_list) # good luck
+    #return(list(map(lambda x:  [x for x in indices], indices)))
