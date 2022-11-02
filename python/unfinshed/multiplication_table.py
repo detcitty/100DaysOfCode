c54ba8428ca165aa89dc1e2d1d3ca4b06e963ca0
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-11-02 11:28:01 -0600

    coding

--- python/unfinshed/multiplication_table.py
+++ python/unfinshed/multiplication_table.py
@@ -14,8 +14,6 @@ for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 '''
 def multiplication_table(size):
     indices = [x for x in range(1, size+1)]
-    new_list = [y*x for x in indices for y in indices]
-    #new_list = [y*x for x in indices for y in indices]
+    new_list = [[y*x for x in indices] for y in indices]
 
     return(new_list) # good luck
-    #return(list(map(lambda x:  [x for x in indices], indices)))
