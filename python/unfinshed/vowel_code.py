60693ae37f736375b86cc522405e51edca697ef5
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-11-23 10:32:18 -0700

    Daily coding

--- python/unfinshed/vowel_code.py
+++ python/unfinshed/vowel_code.py
@@ -15,12 +15,19 @@ Step 2: Now create a function called decode() to turn the numbers back into vowe
 
 For example, decode("h3 th2r2") would return "hi there".
 
-For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+For the sake of simplicity, you can assume that any numbers passed into the function wrrtg......;ill correspond to vowels.
 
 '''
+import regex as re
+
 
 def encode(st):
+    vowel_dict = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    #[x for x in enumerate(vowel_dict)]
+    for count, (k, v) in enumerate(vowel_dict.items()):
+        re.sub(f"{v}")
     return
-    
+
+
 def decode(st):
     return
