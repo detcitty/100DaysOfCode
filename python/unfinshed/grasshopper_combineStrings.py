f8c70dc99b6c12c7505490efd19dbd794f44fca2
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-12-16 15:48:47 -0700

    more updates taking place

---
+++ python/unfinshed/grasshopper_combineStrings.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/55f73f66d160f1f1db000059/train/python
+
+# Create the combine_names function here
+'''
+Combine strings function
+Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combine_names('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+'''
+
+def combine_names(first_name, last_name):
+    return(f"{first_name} {last_name}")
