7afff50127c9ea6c889c2211b313ff3444ae9ad7
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-12-16 16:16:35 -0700

    help

--- python/unfinshed/formatting_decimal_places.py
+++ python/unfinshed/formatting_decimal_places.py
@@ -10,5 +10,6 @@ Example:
 '''
 
 def two_decimal_places(n):
-    raise NotImplementedError("TODO: two_decimal_places")
+    #raise NotImplementedError("TODO: two_decimal_places")
+    return(float(f"{n:.2f}"))
 
