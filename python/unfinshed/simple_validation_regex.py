12dbf05f9fc0460aeb3d6a8d0c3f0322b868a7c1
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-12-19 10:52:46 -0700

    ok

---
+++ python/unfinshed/simple_validation_regex.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/train/python
+
+'''
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+'''
+import regex as re
+
+def validate_usr(username):
+    #your code here
+    #pattern = r'^(?![a-z]|\d|\_).*'
+    pattern = r'([a-z\d_]){4,16}'
+
+    return(True if re.match(username, pattern) else False)
