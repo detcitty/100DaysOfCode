bf4de2455f2d726c8881b676e2b20b3e44a4867a
Devin Etcitty <devin.etcitty@hci.utah.edu>
2022-12-29 14:48:44 -0700

    updating ideas

--- python/unfinshed/summixedarray.py
+++ python/unfinshed/summixedarray.py
@@ -1,9 +1,14 @@
 # https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/python
 
+'''
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+'''
 def sum_mix(arr):
     #your code here
     # turn all values into numbers
-    numbers = list(map(lambda x: int(x) if x == type(str) else x, arr))
+    numbers = list(map(lambda x: int(x) if isinstance(x, str) else x, arr))
     return(sum(numbers))
 
 test1 = sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])
