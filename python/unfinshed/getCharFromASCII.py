c71c08672204120c6f748d2328d892df12a017d6
Devin Etcitty <devin.etcitty@hci.utah.edu>
2023-01-05 13:13:31 -0700

    updating more stuff

---
+++ python/unfinshed/getCharFromASCII.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+
+'''
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+should return:
+
+'A'
+For ASCII table, you can refer to http://www.asciitable.com/
+
+
+'''
+
+def get_char(c):
+  # Your code goes here ^_^
+  return
