a8164cfe4c6cb5f37d0f3852219b3cfafc0a9812
Devin Etcitty <devin.etcitty@hci.utah.edu>
2023-01-05 12:57:45 -0700

    more working issues

--- python/unfinshed/dig_ng.py
+++ python/unfinshed/dig_ng.py
@@ -10,16 +10,25 @@ Call nb_dig (or nbDig or ...) the function taking n and d as parameters and retu
 '''
 import numpy as np
 
+
 def nb_dig(n, d):
     # your code
     values = np.arange(0, n+1)
     k_squared = np.square(values)
+    #print(k_squared)
     # I just need to check if there is at least one digit d in the k**2 list
-    str_k = np.array(map(str, k_squared))
+    # How do I convert an array to string
+    str_k = np.char.mod('%d', k_squared)
+    #(str_k)
+    #str_k = np.array(map(str, k_squared))
     count_d = np.char.count(str_k, str(d))
-    print(count_d)
-    return(np.count_nonzero(count_d))
+    #print(count_d)
+    return(np.sum(count_d))
+
 
 # Something is not working right.
 # How can I do an apply on all of the numbers?
 test1 = nb_dig(5750, 0)
+test2 = nb_dig(10, 0)
+
+print(test2)
