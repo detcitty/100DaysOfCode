d0db5f8a45e5ba65021705bbc8e2b2214904cd6a
Devin Etcitty <devin.etcitty@hci.utah.edu>
2023-01-09 10:50:27 -0700

    coding

--- python/unfinshed/countCharsString.py
+++ python/unfinshed/countCharsString.py
@@ -1,9 +1,26 @@
 # https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/python
 '''
-def count(string):
-    # The function code should be here
-    return {}
+The main idea is to count all the occurring characters in a string. If you
+have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object 
+literal, {}.
 '''
+from collections import defaultdict
+
 def count(string):
     # The function code should be here
-    return {}
+    #all_chars = string.split("")
+    all_chars = list(string)
+
+    unique_chars = set(all_chars)
+    intial_dict = {}
+    counts_char = defaultdict(lambda: 0, intial_dict)
+    
+    for x in all_chars:
+        key = x
+        value = counts_char[x]
+        
+        counts_char[key] = value + 1
+        
+    return {counts_char}
