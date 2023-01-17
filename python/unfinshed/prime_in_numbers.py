1328bf4de5d155dee7fbae84eeadadf5c1a5d64d
Devin Etcitty <devin.etcitty@gmail.com>
2023-01-17 10:48:57 -0700

    testing

--- python/unfinshed/prime_in_numbers.py
+++ python/unfinshed/prime_in_numbers.py
@@ -1,16 +1,28 @@
 # https://www.codewars.com/kata/54d512e62a5e54c96200019e/train/python
 '''
-Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
+Given a positive number n > 1 find the prime factor decomposition of n. 
+The result will be a string with the following form :
 
  "(p1**n1)(p2**n2)...(pk**nk)"
 with the p(i) in increasing order and n(i) empty if n(i) is 1.
 
 Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+
 '''
+import numpy as np
+from sympy import *
 
 def find_next_prime(n):
+    next = n+1
+    found_prime = False
+    while not found_prime:
+        check_prime = isprime(found_prime)
 
-    return()
+        if check_prime:
+            found_prime = True
+        else:
+            next += 1
+    return(next)
 
 
 def prime_factors(n):
