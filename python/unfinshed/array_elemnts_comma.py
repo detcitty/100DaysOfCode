1af5d4a5e64c750f2628eea5086d71e873289628
Devin Etcitty <devin.etcitty@gmail.com>
2023-05-12 11:02:04 -0600

    Document

--- python/unfinshed/array_elemnts_comma.py
+++ python/unfinshed/array_elemnts_comma.py
@@ -1,6 +1,18 @@
 # https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/python
 
+'''
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+'''
+
 def print_array(arr):
     #your code here
 
-    return(",".join(arr))
+    return(",".join(arr) if type(arr) == list else arr)
