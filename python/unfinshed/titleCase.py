e72532f101ca3d84d18a5777a1cbe2951b5204c4
Devin Etcitty <devin.etcitty@gmail.com>
2023-05-24 14:41:09 -0600

    Update'

--- python/unfinshed/titleCase.py
+++ python/unfinshed/titleCase.py
@@ -7,14 +7,7 @@ Second argument (optional):
 - space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
 
 '''
-def title_case(title, minor_words=''):
-    title_list = title.split(' ')
-    title_lowercase = list(map(lambda x: x.lower(), title_list))
-    minor_words_list = [] if len(minor_words) > 0  else minor_words.split(' ')
-    """
-    my function
-    :param value: value for my function; default is 1
-    """
+def deadcode(title, minor_words=''):
 
     if minor_words is title_case.__defaults__[0]:
         print('default')
@@ -25,3 +18,20 @@ def title_case(title, minor_words=''):
     print(minor_words)
     pass
     
+
+def title_case(title, minor_words=''):
+    title_list = title.split(' ')
+    title_lowercase = list(map(lambda x: x.lower(), title_list))
+    minor_words_list = [] if len(minor_words) > 0  else minor_words.split(' ')
+    """
+    my function
+    :param value: value for my function; default is 1
+    """
+    new_title = ''
+
+    for x in title_lowercase:
+        new_title += x.capitalize() + " "
+    
+    new_title = new_title.strip(" ")
+    return(new_title)
+
