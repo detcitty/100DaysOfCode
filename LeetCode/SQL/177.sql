f833beba9222939404fec7c7a3818531c785a1d0
Devin Etcitty <devin.etcitty@gmail.com>
2026-09-22 21:36:15 -0700

    more

--- LeetCode/SQL/177.sql
+++ LeetCode/SQL/177.sql
@@ -4,6 +4,7 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
+      --SELECT id, salary from Employee ORDER BY salary DESC LIMIT 1 OFFSET N-1;
 
   );
 END
