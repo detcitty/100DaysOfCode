ed3b45a7ce5b2299f55acbb940c886bad0da5e58
Devin Etcitty <devin.etcitty@gmail.com>
2026-09-22 21:28:31 -0700

    changes

---
+++ LeetCode/SQL/177.sql
@@ -0,0 +1,9 @@
+--https://leetcode.com/problems/nth-highest-salary/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+
+  );
+END
